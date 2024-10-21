5702d3accbd98babc3f59ba37bd5bb17cffe7151
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 12:53:46 +0200

    Added type annotations.

--- classes/Constraint/class_Constraint.py
+++ classes/Constraint/class_Constraint.py
@@ -1,7 +1,12 @@
+from typing import Callable
+
+import torch
+
+
 class Constraint:
 
-    def __init__(self, function):
+    def __init__(self, function: Callable):
         self.function = function
 
-    def __call__(self, x, *args, **kwargs):
+    def __call__(self, x: torch.Tensor, *args, **kwargs) -> bool:
         return self.function(x, *args, **kwargs)
