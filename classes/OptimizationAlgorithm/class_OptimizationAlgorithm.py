4fb401696b6eca7674fc0de4f7fbca5942f52905
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 09:52:50 +0200

    Added first features to 'OptimizationAlgorithm' with corresponding tests.

--- classes/OptimizationAlgorithm/class_OptimizationAlgorithm.py
+++ classes/OptimizationAlgorithm/class_OptimizationAlgorithm.py
@@ -1,2 +1,14 @@
 import torch
 
+
+class OptimizationAlgorithm:
+
+    def __init__(self, implementation, initial_state):
+        self.implementation = implementation
+        self.initial_state = initial_state
+
+    def get_initial_state(self):
+        return self.initial_state
+
+    def get_implementation(self):
+        return self.implementation
--- tests/OptimizationAlgorithm/test_OptimizationAlgorithm.py
+++ tests/OptimizationAlgorithm/test_OptimizationAlgorithm.py
@@ -1,11 +1,25 @@
 import unittest
+
+import algorithms.dummy
 from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
+from algorithms.dummy import Dummy
+import torch
 
 
 class TestClassOptimizationAlgorithm(unittest.TestCase):
 
     def setUp(self):
-        self.optimization_algorithm = OptimizationAlgorithm()
+        self.dim = torch.randint(low=1, high=1000, size=(1,)).item()
+        self.length_state = torch.randint(low=1, high=5, size=(1,)).item()
+        self.initial_state = torch.randn(size=(self.length_state, self.dim))
+        self.optimization_algorithm = OptimizationAlgorithm(implementation=Dummy(), initial_state=self.initial_state)
 
     def test_creation(self):
         self.assertIsInstance(self.optimization_algorithm, OptimizationAlgorithm)
+
+    def test_get_initial_state(self):
+        self.assertIsInstance(self.optimization_algorithm.get_initial_state(), torch.Tensor)
+        self.assertTrue(torch.equal(self.optimization_algorithm.get_initial_state(), self.initial_state))
+
+    def test_get_implementation(self):
+        self.assertIsInstance(self.optimization_algorithm.get_implementation(), algorithms.dummy.Dummy)
