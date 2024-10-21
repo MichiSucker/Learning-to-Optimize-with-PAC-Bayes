87ca8200fa5a702dec26f236307a2061608a1b05
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 10:51:06 +0200

    Finished basic implementation of class 'OptimizationAlgorithm' and corresponding tests. To proceed, I first have to continue with class 'LossFunction'.

--- classes/OptimizationAlgorithm/class_OptimizationAlgorithm.py
+++ classes/OptimizationAlgorithm/class_OptimizationAlgorithm.py
@@ -5,10 +5,41 @@ class OptimizationAlgorithm:
 
     def __init__(self, implementation, initial_state):
         self.implementation = implementation
-        self.initial_state = initial_state
+        self.initial_state = initial_state.clone()
+        self.current_state = initial_state.clone()
+        self.current_iterate = self.current_state[-1]
+        self.iteration_counter = 0
 
     def get_initial_state(self):
         return self.initial_state
 
     def get_implementation(self):
         return self.implementation
+
+    def get_current_state(self):
+        return self.current_state
+
+    def get_current_iterate(self):
+        return self.current_iterate
+
+    def get_iteration_counter(self):
+        return self.iteration_counter
+
+    def set_iteration_counter(self, n):
+        self.iteration_counter = n
+
+    def reset_iteration_counter_to_zero(self):
+        self.iteration_counter = 0
+
+    def reset_to_initial_state(self):
+        self.current_state = self.initial_state.clone()
+
+    def set_current_state(self, new_state):
+        self.current_state = new_state.clone()
+        self.current_iterate = self.current_state[-1]
+
+    def perform_step(self):
+        self.iteration_counter += 1
+        self.current_iterate = self.implementation.forward(self)
+        with torch.no_grad():
+            self.implementation.update_state(self)
--- tests/OptimizationAlgorithm/test_OptimizationAlgorithm.py
+++ tests/OptimizationAlgorithm/test_OptimizationAlgorithm.py
@@ -12,10 +12,17 @@ class TestClassOptimizationAlgorithm(unittest.TestCase):
         self.dim = torch.randint(low=1, high=1000, size=(1,)).item()
         self.length_state = torch.randint(low=1, high=5, size=(1,)).item()
         self.initial_state = torch.randn(size=(self.length_state, self.dim))
+        self.current_state = self.initial_state.clone()
         self.optimization_algorithm = OptimizationAlgorithm(implementation=Dummy(), initial_state=self.initial_state)
 
     def test_creation(self):
         self.assertIsInstance(self.optimization_algorithm, OptimizationAlgorithm)
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.optimization_algorithm, 'initial_state'))
+        self.assertTrue(hasattr(self.optimization_algorithm, 'current_state'))
+        self.assertTrue(hasattr(self.optimization_algorithm, 'implementation'))
+        self.assertTrue(hasattr(self.optimization_algorithm, 'iteration_counter'))
 
     def test_get_initial_state(self):
         self.assertIsInstance(self.optimization_algorithm.get_initial_state(), torch.Tensor)
@@ -23,3 +30,49 @@ class TestClassOptimizationAlgorithm(unittest.TestCase):
 
     def test_get_implementation(self):
         self.assertIsInstance(self.optimization_algorithm.get_implementation(), algorithms.dummy.Dummy)
+
+    def test_get_current_state(self):
+        self.assertIsInstance(self.optimization_algorithm.get_current_state(), torch.Tensor)
+        self.assertTrue(torch.equal(self.optimization_algorithm.get_current_state(), self.current_state))
+
+    def test_get_current_iterate(self):
+        self.assertIsInstance(self.optimization_algorithm.get_current_iterate(), torch.Tensor)
+        self.assertTrue(torch.equal(self.optimization_algorithm.get_current_iterate(),
+                                    self.optimization_algorithm.get_current_state()[-1]))
+
+    def test_get_iteration_counter(self):
+        self.assertIsInstance(self.optimization_algorithm.get_iteration_counter(), int)
+
+    def test_set_iteration_counter(self):
+        random_number = torch.randint(low=0, high=100, size=(1,)).item()
+        self.optimization_algorithm.set_iteration_counter(n=random_number)
+        self.assertEqual(self.optimization_algorithm.get_iteration_counter(), random_number)
+
+    def test_reset_iteration_counter_to_zero(self):
+        self.optimization_algorithm.set_iteration_counter(10)
+        self.assertNotEqual(self.optimization_algorithm.get_iteration_counter(), 0)
+        self.optimization_algorithm.reset_iteration_counter_to_zero()
+        self.assertEqual(self.optimization_algorithm.get_iteration_counter(), 0)
+
+    def test_reset_to_initial_state(self):
+        self.optimization_algorithm.reset_to_initial_state()
+        self.assertTrue(torch.equal(self.optimization_algorithm.current_state,
+                                    self.optimization_algorithm.initial_state))
+        self.assertTrue(torch.equal(self.optimization_algorithm.initial_state, self.initial_state))
+
+    def test_set_current_state(self):
+        old_state = self.optimization_algorithm.get_current_state()
+        new_state = torch.randn(size=(self.length_state, self.dim))
+        self.optimization_algorithm.set_current_state(new_state)
+        self.assertFalse(torch.equal(old_state, self.optimization_algorithm.get_current_state()))
+        self.assertTrue(torch.equal(new_state, self.optimization_algorithm.get_current_state()))
+        self.assertTrue(torch.equal(new_state[-1], self.optimization_algorithm.get_current_iterate()))
+
+    def test_update_state(self):
+        self.assertTrue(hasattr(self.optimization_algorithm.implementation, 'forward'))
+        self.assertTrue(hasattr(self.optimization_algorithm.implementation, 'update_state'))
+        current_state = self.optimization_algorithm.get_current_state().clone()
+        current_iteration_counter = self.optimization_algorithm.get_iteration_counter()
+        self.optimization_algorithm.perform_step()
+        self.assertNotEqual(self.optimization_algorithm.iteration_counter, current_iteration_counter)
+        self.assertFalse(torch.equal(self.optimization_algorithm.get_current_state(), current_state))
