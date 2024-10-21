b6123e7e7f8a654b24389d20c0b4edc0d8a61c9c
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 10:51:52 +0200

    Adjusted implementation to include functioning 'forward'- and 'update_state'-methods.

--- algorithms/dummy.py
+++ algorithms/dummy.py
@@ -6,10 +6,12 @@ class Dummy(nn.Module):
 
     def __init__(self):
         super(Dummy, self).__init__()
+        self.scale = 1.
 
-    def forward(self):
-        pass
+    def forward(self, optimization_algorithm):
+        current_iterate = optimization_algorithm.get_current_iterate()
+        return current_iterate + self.scale * torch.randn(size=current_iterate.shape)
 
     @staticmethod
-    def update_state(opt_algo):
-        pass
+    def update_state(optimization_algorithm):
+        optimization_algorithm.current_state = optimization_algorithm.current_iterate.detach().clone().reshape((1, -1))
