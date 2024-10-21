e345f1684c49e29b93f85b09827f5c995210e807
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 08:38:03 +0200

    Setup simple test and coverage environment. Will be adjusted, when more code is added.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import unittest
+import coverage
 
 
 if __name__ == '__main__':
-    raise NotImplementedError
+
+    cov = coverage.Coverage()
+    cov.start()
+
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover(start_dir='tests')
+    runner = unittest.runner.TextTestRunner().run(test_suite)
+
+    cov.stop()
+    cov.save()
+    cov.report()
