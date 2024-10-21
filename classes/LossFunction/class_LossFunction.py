35af58940b2fd5c7f14ed89cb86ef86573719410
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 13:02:04 +0200

    Adjusted type annotations.

--- classes/LossFunction/class_LossFunction.py
+++ classes/LossFunction/class_LossFunction.py
@@ -8,10 +8,10 @@ class LossFunction:
     def __init__(self, function: Callable):
         self.function = function
 
-    def __call__(self, x: torch.Tensor, *args: list, **kwargs: dict) -> torch.Tensor:
+    def __call__(self, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         return self.function(x, *args, **kwargs)
 
-    def compute_gradient(self, x: torch.Tensor, *args: list, **kwargs: dict) -> torch.Tensor:
+    def compute_gradient(self, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         y = x.clone().detach().requires_grad_(True)
         function_value = self.function(y, *args, **kwargs)
         # If the gradient of y already got compute inside the function call (which might be necessary sometimes)
