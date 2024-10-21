ad2d781ffe419e66e8283fc1e5209456c91dea3f
Michael Sucker <michael_sucker@t-online.de>
2024-10-21 11:09:14 +0200

    Added implementation of class 'LossFunction' with corresponding tests.

---
+++ tests/LossFunction/test_LossFunction.py
@@ -0,0 +1,26 @@
+import unittest
+import torch
+from classes.LossFunction.class_LossFunction import LossFunction
+
+
+def dummy_function(x):
+    return 0.5 * torch.linalg.norm(x) ** 2
+
+
+class TestLossFunction(unittest.TestCase):
+
+    def setUp(self):
+        self.loss_function = LossFunction(function=dummy_function)
+
+    def test_creation(self):
+        self.assertIsInstance(self.loss_function, LossFunction)
+
+    def test_call_function(self):
+        dimension = torch.randint(low=1, high=50, size=(1,)).item()
+        random_point = torch.randn(size=(dimension,))
+        self.assertTrue(torch.equal(self.loss_function(random_point), dummy_function(random_point)))
+
+    def test_compute_gradient(self):
+        dimension = torch.randint(low=1, high=50, size=(1,)).item()
+        random_point = torch.randn(size=(dimension,))
+        self.assertTrue(torch.allclose(self.loss_function.compute_gradient(random_point), random_point))
