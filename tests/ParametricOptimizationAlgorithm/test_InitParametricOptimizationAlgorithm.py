f3dc3bd4fa52248b1fa229999abdd937e8716b68
Michael Sucker <michael_sucker@t-online.de>
2024-10-23 14:21:46 +0200

    Initiated file 'test_InitParametricOptimizationAlgorithm.py'

---
+++ tests/ParametricOptimizationAlgorithm/test_InitParametricOptimizationAlgorithm.py
@@ -0,0 +1,7 @@
+import unittest
+from classes.OptimizationAlgorithm.derived_classes.subclass_ParametricOptimizationAlgorithm import (
+    ParametricOptimizationAlgorithm)
+
+
+class TestInitParametricOptimizationAlgorithm(unittest.TestCase):
+    pass
