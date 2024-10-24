cf525cf8dcecc51067d6a0dd6419fa5e02a76ee1
Michael Sucker <michael_sucker@t-online.de>
2024-10-24 08:55:47 +0200

    Instantiated test-file.

---
+++ tests/ParametricOptimizationAlgorithm/test_SamplingParametricOptimizationAlgorithm.py
@@ -0,0 +1,7 @@
+import unittest
+from classes.OptimizationAlgorithm.derived_classes.subclass_ParametricOptimizationAlgorithm import (
+    ParametricOptimizationAlgorithm)
+
+
+class TestSamplingParametricOptimizationAlgorithm:
+    pass
