163845c9dc10db766cb03a6837fad2e8e898694b
Michael Sucker <michael_sucker@t-online.de>
2024-10-24 07:53:48 +0200

    Added non-trainable version, which is needed for initialization. Also changed step-size; otherwise training (just the step-size!) is not really possible.

--- algorithms/dummy.py
+++ algorithms/dummy.py
@@ -6,7 +6,22 @@ class Dummy(nn.Module):
 
     def __init__(self):
         super(Dummy, self).__init__()
-        self.scale = nn.Parameter(torch.tensor(1.))
+        self.scale = nn.Parameter(torch.tensor(1e-3))
+
+    def forward(self, optimization_algorithm):
+        gradient = optimization_algorithm.loss_function.compute_gradient(optimization_algorithm.current_iterate)
+        return optimization_algorithm.current_iterate + self.scale * gradient
+
+    @staticmethod
+    def update_state(optimization_algorithm):
+        optimization_algorithm.current_state = optimization_algorithm.current_iterate.detach().clone().reshape((1, -1))
+
+
+class NonTrainableDummy(nn.Module):
+
+    def __init__(self):
+        super(NonTrainableDummy, self).__init__()
+        self.scale = torch.tensor(1e-4)
 
     def forward(self, optimization_algorithm):
         gradient = optimization_algorithm.loss_function.compute_gradient(optimization_algorithm.current_iterate)
