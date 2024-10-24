c3212bf60cd3488cfc80d606d91ee03fd58ce4b1
Michael Sucker <michael_sucker@t-online.de>
2024-10-24 11:50:33 +0200

    Renamed setter of variable 'check_constraint_every'.

--- classes/OptimizationAlgorithm/derived_classes/subclass_ParametricOptimizationAlgorithm.py
+++ classes/OptimizationAlgorithm/derived_classes/subclass_ParametricOptimizationAlgorithm.py
@@ -93,7 +93,7 @@ class ConstraintChecker:
     def set_variable__there_is_a_constraint__to(self, new_bool: bool):
         self.there_is_a_constraint = new_bool
 
-    def set_variable_check_constraint_every_to(self, new_number: int):
+    def set_variable__check_constraint_every__to(self, new_number: int):
         self.check_constraint_every = new_number
 
     def update_point_inside_constraint_or_reject(self, optimization_algorithm: OptimizationAlgorithm):
