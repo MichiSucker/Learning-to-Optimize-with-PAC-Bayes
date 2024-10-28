94d41d990804fde66c9cb6c05089317a68c548ba
Michael Sucker <michael_sucker@t-online.de>
2024-10-28 09:01:23 +0100

    Implemented 'natural_parameters' and corresponding test.

---
+++ tests/test_natural_parameters.py
@@ -0,0 +1,17 @@
+import unittest
+
+import torch
+
+from natural_parameters.natural_parameters import evaluate_natural_parameters_at
+
+
+class TestNaturalParameters(unittest.TestCase):
+
+    def test_evaluate_natural_parameters_at(self):
+        x = torch.rand(size=(1,))
+        result = evaluate_natural_parameters_at(x)
+        self.assertTrue(torch.equal(result, torch.tensor([x, -0.5 * x ** 2])))
+        with self.assertRaises(TypeError):
+            evaluate_natural_parameters_at(1)
+        with self.assertRaises(ValueError):
+            evaluate_natural_parameters_at(torch.tensor([1., 2.]))
