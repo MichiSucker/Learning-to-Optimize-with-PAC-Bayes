2171c5b81e43bc205d660c30e0b28a3e3ab86aad
Michael Sucker <michael_sucker@t-online.de>
2024-10-30 08:49:19 +0100

    Copied NNOptimizer from old implementation.

---
+++ algorithms/nn_optimizer.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+
+
+class NnOptimizer(nn.Module):
+
+    def __init__(self, dim: int):
+        super(NnOptimizer, self).__init__()
+
+        self.extrapolation = nn.Parameter(0.001 * torch.ones(dim))
+        self.gradient = nn.Parameter(0.001 * torch.ones(dim))
+        self.update_layer = nn.Sequential(
+            nn.Conv2d(6, 16, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 1, kernel_size=1),
+        )
+
+        self.coefficients = nn.Sequential(
+            nn.Linear(5, 8),
+            nn.ReLU(),
+            nn.Linear(8, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+        )
+
+        # For stability
+        self.eps = torch.tensor(1e-10).float()
+
+    def forward(self, opt_algo):
+
+        # Normalize gradient
+        grad = opt_algo.loss_function.grad(opt_algo.current_state[1])
+        grad_norm = torch.linalg.norm(grad).reshape((1,))
+
+        if grad_norm > self.eps:
+            grad = grad / grad_norm
+
+        # Compute new hidden state
+        diff = opt_algo.current_state[1] - opt_algo.current_state[0]
+        diff_norm = torch.linalg.norm(diff).reshape((1,))
+
+        if diff_norm > self.eps:
+            diff = diff / diff_norm
+
+        coefficients = self.coefficients(
+            torch.concat(
+                (torch.log(1 + grad_norm.reshape((1,))),
+                 torch.log(1 + diff_norm.reshape((1,))),
+                 torch.dot(grad, diff).reshape((1,)),
+                 torch.log(opt_algo.loss_function(opt_algo.current_state[1]).detach().reshape((1,))),
+                 torch.log(opt_algo.loss_function(opt_algo.current_state[0]).detach().reshape((1,)))))
+        )
+        direction = self.update_layer(torch.concat((coefficients[0] * self.gradient * grad.reshape((1, 1, 1, -1)),
+                                                    coefficients[1] * self.extrapolation * diff.reshape((1, 1, 1, -1)),
+                                                    coefficients[2] * grad.reshape((1, 1, 1, -1)),
+                                                    coefficients[3] * diff.reshape((1, 1, 1, -1)),
+                                                    opt_algo.current_state[0].reshape((1, 1, 1, -1)),
+                                                    opt_algo.current_state[1].reshape((1, 1, 1, -1)),
+                                                    ),
+                                                   dim=1)).flatten()
+
+        return opt_algo.current_state[-1] + direction
+
+    @staticmethod
+    def update_state(opt_algo):
+        opt_algo.current_state[0] = opt_algo.current_state[1].detach().clone()
+        opt_algo.current_state[1] = opt_algo.current_iterate.detach().clone()
