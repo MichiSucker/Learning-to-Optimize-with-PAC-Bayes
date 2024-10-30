Bug-fix: Used old method-name for gradient computation. + Some reformatting.

algorithms/nn_optimizer.py
@@ -34,19 +34,17 @@ class NnOptimizer(nn.Module):
         # For stability
         self.eps = torch.tensor(1e-10).float()
 
-    def forward(self, opt_algo):
+    def forward(self, algorithm):
 
         # Normalize gradient
-        grad = opt_algo.loss_function.grad(opt_algo.current_state[1])
+        grad = algorithm.loss_function.compute_gradient(algorithm.current_state[1])
         grad_norm = torch.linalg.norm(grad).reshape((1,))
-
         if grad_norm > self.eps:
             grad = grad / grad_norm
 
         # Compute new hidden state
-        diff = opt_algo.current_state[1] - opt_algo.current_state[0]
+        diff = algorithm.current_state[1] - algorithm.current_state[0]
         diff_norm = torch.linalg.norm(diff).reshape((1,))
-
         if diff_norm > self.eps:
             diff = diff / diff_norm
 
@@ -55,19 +53,20 @@ class NnOptimizer(nn.Module):
                 (torch.log(1 + grad_norm.reshape((1,))),
                  torch.log(1 + diff_norm.reshape((1,))),
                  torch.dot(grad, diff).reshape((1,)),
-                 torch.log(opt_algo.loss_function(opt_algo.current_state[1]).detach().reshape((1,))),
-                 torch.log(opt_algo.loss_function(opt_algo.current_state[0]).detach().reshape((1,)))))
+                 torch.log(algorithm.loss_function(algorithm.current_state[1]).detach().reshape((1,))),
+                 torch.log(algorithm.loss_function(algorithm.current_state[0]).detach().reshape((1,)))))
         )
-        direction = self.update_layer(torch.concat((coefficients[0] * self.gradient * grad.reshape((1, 1, 1, -1)),
-                                                    coefficients[1] * self.extrapolation * diff.reshape((1, 1, 1, -1)),
-                                                    coefficients[2] * grad.reshape((1, 1, 1, -1)),
-                                                    coefficients[3] * diff.reshape((1, 1, 1, -1)),
-                                                    opt_algo.current_state[0].reshape((1, 1, 1, -1)),
-                                                    opt_algo.current_state[1].reshape((1, 1, 1, -1)),
-                                                    ),
-                                                   dim=1)).flatten()
+        direction = self.update_layer(
+            torch.concat((
+                coefficients[0] * self.gradient * grad.reshape((1, 1, 1, -1)),
+                coefficients[1] * self.extrapolation * diff.reshape((1, 1, 1, -1)),
+                coefficients[2] * grad.reshape((1, 1, 1, -1)),
+                coefficients[3] * diff.reshape((1, 1, 1, -1)),
+                algorithm.current_state[0].reshape((1, 1, 1, -1)),
+                algorithm.current_state[1].reshape((1, 1, 1, -1)),
+                ), dim=1)).flatten()
 
-        return opt_algo.current_state[-1] + direction
+        return algorithm.current_state[-1] + direction
 
     @staticmethod
     def update_state(opt_algo):