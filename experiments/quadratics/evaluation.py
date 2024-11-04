9afed83697b2b5e1884a747f684e6b2ce484e438
Michael Sucker <michael_sucker@t-online.de>
2024-11-04 12:24:24 +0100

    Implemented evaluation for quadratics.

---
+++ experiments/quadratics/evaluation.py
@@ -0,0 +1,216 @@
+import numpy as np
+import torch
+import pickle
+from pathlib import Path
+import time
+from algorithms.heavy_ball import HeavyBallWithFriction
+from classes.LossFunction.derived_classes.subclass_ParametricLossFunction import ParametricLossFunction
+from experiments.quadratics.algorithm import Quadratics
+from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
+from experiments.quadratics.data_generation import get_loss_function_of_algorithm
+from experiments.quadratics.training import get_describing_property, get_baseline_algorithm
+
+
+class EvaluationAssistant:
+
+    def __init__(self,
+                 test_set,
+                 loss_of_algorithm,
+                 initial_state,
+                 number_of_iterations_during_training,
+                 optimal_hyperparameters,
+                 implementation_class):
+        self.test_set = test_set
+        self.initial_state = initial_state
+        self.number_of_iterations_during_training = number_of_iterations_during_training
+        self.number_of_iterations_for_testing = 2 * number_of_iterations_during_training
+        self.loss_of_algorithm = loss_of_algorithm
+        self.optimal_hyperparameters = optimal_hyperparameters
+        self.implementation_class = implementation_class
+        self.dim = initial_state.shape[1]
+        self.implementation_arguments = None
+        self.strong_convexity_parameter = None
+        self.smoothness_parameter = None
+
+    def set_up_learned_algorithm(self, arguments_of_implementation_class):
+        if arguments_of_implementation_class is None:
+            learned_algorithm = OptimizationAlgorithm(
+                implementation=self.implementation_class(),
+                initial_state=self.initial_state,
+                loss_function=ParametricLossFunction(function=self.loss_of_algorithm,
+                                                     parameter=self.test_set[0]))
+        elif (arguments_of_implementation_class is not None) and (self.implementation_arguments is not None):
+            learned_algorithm = OptimizationAlgorithm(
+                implementation=self.implementation_class(self.implementation_arguments),
+                initial_state=self.initial_state,
+                loss_function=ParametricLossFunction(function=self.loss_of_algorithm,
+                                                     parameter=self.test_set[0]))
+        else:
+            raise Exception("Arguments of implementation are not specified correctly.")
+        learned_algorithm.implementation.load_state_dict(self.optimal_hyperparameters)
+        return learned_algorithm
+
+
+def evaluate_algorithm(loading_path, path_of_experiment):
+    evaluation_assistant = set_up_evaluation_assistant(loading_path)
+    learned_algorithm = evaluation_assistant.set_up_learned_algorithm(
+        arguments_of_implementation_class=evaluation_assistant.implementation_arguments)
+    baseline_algorithm = get_baseline_algorithm(
+        loss_function=learned_algorithm.loss_function, smoothness_constant=evaluation_assistant.smoothness_parameter,
+        strong_convexity_constant=evaluation_assistant.smoothness_parameter, dim=evaluation_assistant.dim)
+
+    losses_of_baseline, losses_of_learned_algorithm, percentage_constrained_satisfied = compute_losses(
+        evaluation_assistant=evaluation_assistant, learned_algorithm=learned_algorithm,
+        baseline_algorithm=baseline_algorithm
+    )
+
+    times_of_learned_algorithm, times_of_baseline_algorithm = compute_times(
+        learned_algorithm=learned_algorithm, baseline_algorithm=baseline_algorithm,
+        evaluation_assistant=evaluation_assistant,
+        stop_procedure_after_at_most=5, ground_truth_losses=[0. for _ in range(len(evaluation_assistant.test_set))])
+
+    save_data(savings_path=create_folder_for_storing_data(path_of_experiment),
+              times_of_learned_algorithm=times_of_learned_algorithm,
+              losses_of_learned_algorithm=losses_of_learned_algorithm,
+              times_of_baseline_algorithm=times_of_baseline_algorithm,
+              losses_of_baseline_algorithm=losses_of_baseline,
+              ground_truth_losses=[0. for _ in range(len(evaluation_assistant.test_set))],
+              percentage_constrained_satisfied=percentage_constrained_satisfied)
+
+
+def load_data(loading_path):
+    pac_bound = np.load(loading_path + 'pac_bound.npy')
+    initial_state = torch.tensor(np.load(loading_path + 'initialization.npy'))
+    n_train = np.load(loading_path + 'number_of_iterations.npy')
+    smoothness_parameter = np.load(loading_path + 'smoothness_parameter.npy')
+    strong_convexity_parameter = np.load(loading_path + 'strong_convexity_parameter.npy')
+    with open(loading_path + 'parameters_problem', 'rb') as file:
+        parameters = pickle.load(file)
+    with open(loading_path + 'samples', 'rb') as file:
+        samples = pickle.load(file)
+    with open(loading_path + 'best_sample', 'rb') as file:
+        best_sample = pickle.load(file)
+    return (pac_bound, initial_state, n_train, parameters, samples, best_sample,
+            strong_convexity_parameter, smoothness_parameter)
+
+
+def create_folder_for_storing_data(path_of_experiment):  # pragma: no cover
+    savings_path = path_of_experiment + "/data/"
+    Path(savings_path).mkdir(parents=True, exist_ok=True)
+    return savings_path
+
+
+def set_up_evaluation_assistant(loading_path):
+    (pac_bound, initial_state, n_train, parameters, samples, best_sample, strong_convexity_parameter,
+     smoothness_parameter) = load_data(loading_path)
+    loss_of_algorithm = get_loss_function_of_algorithm()
+
+    evaluation_assistant = EvaluationAssistant(test_set=parameters['test'], loss_of_algorithm=loss_of_algorithm,
+                                               initial_state=torch.tensor(initial_state),
+                                               number_of_iterations_during_training=n_train,
+                                               optimal_hyperparameters=best_sample, implementation_class=Quadratics)
+    evaluation_assistant.implementation_arguments = initial_state.shape[1]
+    evaluation_assistant.smoothness_parameter = torch.tensor(smoothness_parameter)
+    evaluation_assistant.strong_convexity_parameter = torch.tensor(strong_convexity_parameter)
+    return evaluation_assistant
+
+
+def save_data(savings_path, times_of_learned_algorithm, losses_of_learned_algorithm, times_of_baseline_algorithm,
+              losses_of_baseline_algorithm, ground_truth_losses, percentage_constrained_satisfied):
+
+    with open(savings_path + 'times_of_learned_algorithm', 'wb') as file:
+        pickle.dump(times_of_learned_algorithm, file)
+    with open(savings_path + 'times_of_baseline_algorithm', 'wb') as file:
+        pickle.dump(times_of_baseline_algorithm, file)
+    np.save(savings_path + 'losses_of_baseline_algorithm', np.array(losses_of_baseline_algorithm))
+    np.save(savings_path + 'losses_of_learned_algorithm', np.array(losses_of_learned_algorithm))
+    np.save(savings_path + 'ground_truth_losses', np.array(ground_truth_losses))
+    np.save(savings_path + 'empirical_probability', percentage_constrained_satisfied)
+
+
+def compute_losses(evaluation_assistant: EvaluationAssistant, learned_algorithm, baseline_algorithm):
+
+    losses_of_baseline_algorithm = []
+    losses_of_learned_algorithm = []
+    number_of_times_constrained_satisfied = 0
+    _, convergence_risk_constraint, _ = get_describing_property()
+
+    for test_parameter in evaluation_assistant.test_set:
+
+        loss_over_iterations = compute_losses_over_iterations(
+            algorithm=learned_algorithm, evaluation_assistant=evaluation_assistant, parameter=test_parameter)
+
+        if does_satisfy_constraint(
+                convergence_risk_constraint=convergence_risk_constraint, loss_at_beginning=loss_over_iterations[0],
+                loss_at_end=loss_over_iterations[evaluation_assistant.number_of_iterations_during_training]):
+
+            number_of_times_constrained_satisfied += 1
+            losses_of_learned_algorithm.append(loss_over_iterations)
+            losses_of_baseline_algorithm.append(compute_losses_over_iterations(
+                algorithm=baseline_algorithm, evaluation_assistant=evaluation_assistant, parameter=test_parameter))
+
+    return (np.array(losses_of_baseline_algorithm),
+            np.array(losses_of_learned_algorithm),
+            number_of_times_constrained_satisfied / len(evaluation_assistant.test_set))
+
+
+def does_satisfy_constraint(convergence_risk_constraint, loss_at_beginning, loss_at_end):
+    return convergence_risk_constraint(loss_at_beginning=loss_at_beginning, loss_at_end=loss_at_end)
+
+
+def compute_losses_over_iterations(algorithm, evaluation_assistant: EvaluationAssistant, parameter):
+    algorithm.reset_state_and_iteration_counter()
+    current_loss_function = ParametricLossFunction(function=evaluation_assistant.loss_of_algorithm, parameter=parameter)
+    algorithm.set_loss_function(current_loss_function)
+    loss_over_iterations = [algorithm.evaluate_loss_function_at_current_iterate().item()]
+    for i in range(evaluation_assistant.number_of_iterations_for_testing):
+        algorithm.perform_step()
+        loss_over_iterations.append(algorithm.evaluate_loss_function_at_current_iterate().item())
+    return loss_over_iterations
+
+
+def time_problem(algorithm, loss_function, maximal_number_of_iterations, optimal_loss, level_of_accuracy):
+
+    algorithm.reset_state_and_iteration_counter()
+    algorithm.set_loss_function(loss_function)
+    current_loss = algorithm.evaluate_loss_function_at_current_iterate()
+    counter = 0
+    start = time.time()
+
+    while ((counter < maximal_number_of_iterations)
+           and ((current_loss - optimal_loss) >= level_of_accuracy)):
+
+        algorithm.perform_step()
+        current_loss = algorithm.evaluate_loss_function_at_current_iterate()
+        counter += 1
+
+    end = time.time()
+    return end - start
+
+
+def compute_times(learned_algorithm: OptimizationAlgorithm,
+                  baseline_algorithm: OptimizationAlgorithm,
+                  evaluation_assistant: EvaluationAssistant,
+                  ground_truth_losses,
+                  stop_procedure_after_at_most):
+
+    levels_of_accuracy = [1e0, 1e-1, 1e-2]
+    times_pac = {epsilon: [0.] for epsilon in levels_of_accuracy}
+    times_std = {epsilon: [0.] for epsilon in levels_of_accuracy}
+
+    for parameter, ground_truth_loss in zip(evaluation_assistant.test_set, ground_truth_losses):
+        for epsilon in levels_of_accuracy:
+
+            cur_loss_function = ParametricLossFunction(function=evaluation_assistant.loss_of_algorithm,
+                                                       parameter=parameter)
+            times_pac[epsilon].append(time_problem(
+                algorithm=learned_algorithm, loss_function=cur_loss_function,
+                maximal_number_of_iterations=stop_procedure_after_at_most, optimal_loss=ground_truth_loss,
+                level_of_accuracy=epsilon))
+
+            times_std[epsilon].append(time_problem(
+                algorithm=baseline_algorithm,loss_function=cur_loss_function,
+                maximal_number_of_iterations=stop_procedure_after_at_most, optimal_loss=ground_truth_loss,
+                level_of_accuracy=epsilon))
+
+    return times_pac, times_std
---
+++ tests/Experiments/CurrentlyWorkingOn/test_evaluation.py
@@ -0,0 +1,123 @@
+import unittest
+import torch
+from experiments.quadratics.algorithm import Quadratics
+from classes.LossFunction.derived_classes.subclass_ParametricLossFunction import ParametricLossFunction
+from experiments.quadratics.evaluation import (load_data,
+                                               create_folder_for_storing_data,
+                                               save_data,
+                                               compute_losses_over_iterations,
+                                               set_up_evaluation_assistant,
+                                               EvaluationAssistant,
+                                               does_satisfy_constraint,
+                                               compute_losses,
+                                               time_problem,
+                                               compute_times,
+                                               evaluate_algorithm)
+from experiments.quadratics.training import get_baseline_algorithm
+from main import TESTING_LEVEL
+
+
+class TestEvaluation(unittest.TestCase):
+
+    def setUp(self):
+        self.path_to_experiment = '/home/michael/Desktop/JMLR_New/Experiments/quadratics/'
+        self.dummy_savings_path = self.path_to_experiment + 'dummy_data/'
+        self.loading_path = self.path_to_experiment + 'data/'
+
+    def test_load_data(self):
+        load_data(self.loading_path)
+
+    def test_create_folder(self):
+        create_folder_for_storing_data(self.path_to_experiment)
+
+    def test_save_data(self):
+        save_data(self.dummy_savings_path,
+                  times_of_learned_algorithm=[],
+                  losses_of_learned_algorithm=[],
+                  times_of_baseline_algorithm=[],
+                  losses_of_baseline_algorithm=[],
+                  ground_truth_losses=[],
+                  percentage_constrained_satisfied=[])
+
+    def test_set_up_evaluation_assistant(self):
+        eval_assist = set_up_evaluation_assistant(loading_path=self.loading_path)
+        self.assertIsInstance(eval_assist, EvaluationAssistant)
+        self.assertIsInstance(eval_assist.implementation_class(eval_assist.implementation_arguments), Quadratics)
+
+    def test_does_satisfy_constraint(self):
+
+        def dummy_constraint(loss_at_beginning, loss_at_end):
+            return loss_at_end < loss_at_beginning
+
+        self.assertTrue(does_satisfy_constraint(convergence_risk_constraint=dummy_constraint,
+                                                loss_at_beginning=10, loss_at_end=1))
+        self.assertFalse(does_satisfy_constraint(convergence_risk_constraint=dummy_constraint,
+                                                 loss_at_beginning=1, loss_at_end=10))
+
+    def test_compute_losses_over_iterations(self):
+        eval_assist = set_up_evaluation_assistant(loading_path=self.loading_path)
+        eval_assist.test_set = eval_assist.test_set[0:2]
+        algorithm = eval_assist.set_up_learned_algorithm(
+            arguments_of_implementation_class=eval_assist.implementation_arguments)
+        losses = compute_losses_over_iterations(algorithm=algorithm,
+                                                evaluation_assistant=eval_assist,
+                                                parameter=eval_assist.test_set[0])
+        self.assertTrue(len(losses), eval_assist.number_of_iterations_for_testing + 1)
+        self.assertEqual(losses[0], eval_assist.loss_of_algorithm(eval_assist.initial_state[-1],
+                                                                  eval_assist.test_set[0]))
+
+    def test_compute_losses(self):
+        eval_assist = set_up_evaluation_assistant(loading_path=self.loading_path)
+        eval_assist.test_set = eval_assist.test_set[0:2]
+        learned_algorithm = eval_assist.set_up_learned_algorithm(
+            arguments_of_implementation_class=eval_assist.implementation_arguments)
+        baseline_algorithm = get_baseline_algorithm(loss_function=learned_algorithm.loss_function,
+                                                    smoothness_constant=eval_assist.smoothness_parameter,
+                                                    strong_convexity_constant=eval_assist.strong_convexity_parameter,
+                                                    dim=eval_assist.dim)
+        losses_of_baseline_algorithm, losses_of_learned_algorithm, percentage = (
+            compute_losses(evaluation_assistant=eval_assist,
+                           learned_algorithm=learned_algorithm,
+                           baseline_algorithm=baseline_algorithm))
+        self.assertEqual(len(losses_of_baseline_algorithm), len(losses_of_learned_algorithm))
+        self.assertTrue(0 <= percentage <= 1)
+
+    def test_time_problem(self):
+        eval_assist = set_up_evaluation_assistant(loading_path=self.loading_path)
+        eval_assist.test_set = eval_assist.test_set[0:2]
+        learned_algorithm = eval_assist.set_up_learned_algorithm(
+            arguments_of_implementation_class=eval_assist.implementation_arguments)
+        loss_function = ParametricLossFunction(function=eval_assist.loss_of_algorithm,
+                                               parameter=eval_assist.test_set[0])
+        time = time_problem(algorithm=learned_algorithm, loss_function=loss_function,
+                            maximal_number_of_iterations=5, optimal_loss=torch.tensor(0.), level_of_accuracy=1.)
+        self.assertIsInstance(time, float)
+
+    def test_compute_times(self):
+
+        eval_assist = set_up_evaluation_assistant(loading_path=self.loading_path)
+        eval_assist.test_set = eval_assist.test_set[0:2]
+        learned_algorithm = eval_assist.set_up_learned_algorithm(
+            arguments_of_implementation_class=eval_assist.implementation_arguments)
+        baseline_algorithm = get_baseline_algorithm(loss_function=learned_algorithm.loss_function,
+                                                    smoothness_constant=eval_assist.smoothness_parameter,
+                                                    strong_convexity_constant=eval_assist.strong_convexity_parameter,
+                                                    dim=eval_assist.dim)
+        times_learned, times_adam = compute_times(
+            learned_algorithm=learned_algorithm, baseline_algorithm=baseline_algorithm,
+            evaluation_assistant=eval_assist, ground_truth_losses=[torch.tensor(0.) for _ in eval_assist.test_set],
+            stop_procedure_after_at_most=5)
+
+        self.assertIsInstance(times_learned, dict)
+        self.assertIsInstance(times_adam, dict)
+        self.assertEqual(len(times_adam.keys()), 3)
+        self.assertEqual(len(times_learned.keys()), 3)
+        for key in times_adam.keys():
+            self.assertEqual(len(times_adam[key]), len(eval_assist.test_set) + 1)
+        for key in times_learned.keys():
+            self.assertEqual(len(times_learned[key]), len(eval_assist.test_set) + 1)
+
+    # @unittest.skipIf(condition=(TESTING_LEVEL != 'FULL_TEST_WITH_EXPERIMENTS'),
+    #                  reason='Too expensive to test all the time.')
+    def test_evaluate_algorithm(self):
+        evaluate_algorithm(loading_path=self.loading_path, path_of_experiment=self.path_to_experiment)
