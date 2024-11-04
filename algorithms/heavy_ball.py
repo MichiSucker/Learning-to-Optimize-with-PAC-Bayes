0863f765c69c8da6e815597d5a1a1a16e8bcd70b
Michael Sucker <michael_sucker@t-online.de>
2024-11-04 08:58:35 +0100

    Added heavy_ball.py

---
+++ algorithms/heavy_ball.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class HB(nn.Module):
+
+    def __init__(self, alpha: torch.tensor, beta: torch.tensor):
+        super(HB, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, opt_algo):
+        return (opt_algo.current_state[1]
+                - self.alpha * opt_algo.loss_function.compute_gradient(opt_algo.current_state[1])
+                + self.beta * (opt_algo.current_state[1] - opt_algo.current_state[0]))
+
+    @staticmethod
+    def update_state(opt_algo):
+        opt_algo.current_state[0] = opt_algo.current_state[1].detach().clone()
+        opt_algo.current_state[1] = opt_algo.current_iterate.detach().clone()
