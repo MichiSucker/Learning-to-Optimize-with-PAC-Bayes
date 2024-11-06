940984c83577b2da97ca0744543a655dfa67c2e2
Michael Sucker <michael_sucker@t-online.de>
2024-11-06 14:59:30 +0100

    Finished implementation of training.

---
+++ experiments/image_processing/training.py
@@ -0,0 +1,230 @@
+from typing import Callable
+import torch
+import numpy as np
+import pickle
+from algorithms.nesterov_accelerated_gradient_descent import NesterovAcceleratedGradient
+from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
+from classes.LossFunction.derived_classes.subclass_ParametricLossFunction import LossFunction, ParametricLossFunction
+from classes.Constraint.class_Constraint import create_list_of_constraints_from_functions
+from classes.Constraint.class_ProbabilisticConstraint import ProbabilisticConstraint
+from classes.OptimizationAlgorithm.derived_classes.derived_classes.subclass_PacBayesOptimizationAlgorithm import \
+    PacBayesOptimizationAlgorithm
+from experiments.image_processing.algorithm import ConvNet
+from experiments.image_processing.data_generation import get_image_height_and_width, get_data
+from describing_property.reduction_property import instantiate_reduction_property_with
+from pathlib import Path
+from natural_parameters.natural_parameters import evaluate_natural_parameters_at
+from sufficient_statistics.sufficient_statistics import evaluate_sufficient_statistics
+
+
+def create_folder_for_storing_data(path_of_experiment):
+    savings_path = path_of_experiment + "/data/"
+    Path(savings_path).mkdir(parents=True, exist_ok=True)
+    return savings_path
+
+
+def get_number_of_datapoints():
+    # TODO: Change number of datapoints to 250 each
+    return {'prior': 25, 'train': 25, 'test': 25, 'validation': 250}
+
+
+def get_parameters_of_estimation():
+    return {'quantile_distance': 0.075, 'quantiles': (0.01, 0.99), 'probabilities': (0.95, 1.0)}
+
+
+def get_update_parameters():
+    return {'num_iter_print_update': 1000,
+            'with_print': True,
+            'bins': [1e6, 1e5, 1e4, 1e3, 1e2, 1e1, 1e0][::-1]}
+
+
+def get_sampling_parameters(maximal_number_of_iterations):
+    length_trajectory = 1
+    restart_probability = length_trajectory / maximal_number_of_iterations
+    return {'lr': torch.tensor(1e-6),
+            'length_trajectory': length_trajectory,
+            'with_restarting': True,
+            'restart_probability': restart_probability,
+            # TODO: Change num_samples to 100
+            'num_samples': 5,
+            'num_iter_burnin': 0}
+
+
+def get_fitting_parameters(maximal_number_of_iterations):
+    length_trajectory = 1
+    restart_probability = length_trajectory / maximal_number_of_iterations
+    return {'restart_probability': restart_probability,
+            'length_trajectory': length_trajectory,
+            # TODO: Rename n_max to number_of_training_iterations
+            # TODO: Change n_max to 200e3
+            'n_max': int(50e3),
+            'lr': 1e-4,
+            'num_iter_update_stepsize': int(20e3),
+            'factor_stepsize_update': 0.5}
+
+
+def get_initialization_parameters():
+    return {'lr': 1e-3, 'num_iter_max': 1000, 'num_iter_print_update': 200, 'num_iter_update_stepsize': 200,
+            'with_print': True}
+
+
+def get_constraint_parameters(number_of_training_iterations):
+    describing_property, _, _ = get_describing_property()
+    return {'describing_property': describing_property,
+            'num_iter_update_constraint': int(number_of_training_iterations // 4)}
+
+
+def get_pac_bayes_parameters(sufficient_statistics):
+    return {'sufficient_statistics': sufficient_statistics,
+            'natural_parameters': evaluate_natural_parameters_at,
+            'covering_number': torch.tensor(75000),
+            'epsilon': torch.tensor(0.05),
+            # TODO: Rename n_max to maximal_number_of_iterations
+            'n_max': 25}
+
+
+def get_describing_property():
+    return instantiate_reduction_property_with(factor=0.2, exponent=1.)
+
+
+def get_dimension_of_optimization_variable():
+    image_height, image_width = get_image_height_and_width()
+    return image_height * image_width
+
+
+def get_initial_states():
+    dim = get_dimension_of_optimization_variable()
+    initial_state_baseline_algorithm = torch.zeros((3, dim))
+    initial_state_learned_algorithm = torch.zeros((2, dim))
+    return initial_state_baseline_algorithm, initial_state_learned_algorithm
+
+
+def get_baseline_algorithm(loss_function_of_algorithm: Callable, smoothness_parameter: float):
+    initial_state_baseline_algorithm, _ = get_initial_states()
+    baseline_algorith = OptimizationAlgorithm(
+        implementation=NesterovAcceleratedGradient(alpha=torch.tensor(1/smoothness_parameter)),
+        initial_state=initial_state_baseline_algorithm,
+        loss_function=LossFunction(function=loss_function_of_algorithm)
+    )
+    return baseline_algorith
+
+
+def get_constraint(loss_functions_for_constraint):
+    parameters_of_estimation = get_parameters_of_estimation()
+    describing_property, _, _ = get_describing_property()
+    list_of_constraints = create_list_of_constraints_from_functions(describing_property=describing_property,
+                                                                    list_of_functions=loss_functions_for_constraint)
+    probabilistic_constraint = ProbabilisticConstraint(list_of_constraints=list_of_constraints,
+                                                       parameters_of_estimation=parameters_of_estimation)
+    return probabilistic_constraint.create_constraint()
+
+
+def get_sufficient_statistics(template_for_loss_function, constants):
+
+    _, convergence_risk_constraint, _ = get_describing_property()
+
+    def sufficient_statistics(optimization_algorithm, parameter, probability):
+        return evaluate_sufficient_statistics(optimization_algorithm=optimization_algorithm,
+                                              parameter_of_loss_function=parameter,
+                                              template_for_loss_function=template_for_loss_function,
+                                              constants=constants,
+                                              convergence_risk_constraint=convergence_risk_constraint,
+                                              convergence_probability=probability)
+
+    return sufficient_statistics
+
+
+def compute_constants_for_sufficient_statistics(loss_functions_for_training):
+    _, initial_state = get_initial_states()
+    _, _, empirical_second_moment = get_describing_property()
+    return empirical_second_moment(list_of_loss_functions=loss_functions_for_training,
+                                   point=initial_state[-1].flatten()) / len(loss_functions_for_training)
+
+
+def get_algorithm_for_learning(loss_function_for_algorithm, loss_functions):
+
+    _, initial_state_learned_algorithm = get_initial_states()
+    image_height, image_width = get_image_height_and_width()
+    constraint = get_constraint(loss_functions_for_constraint=loss_functions['validation'])
+    constants = compute_constants_for_sufficient_statistics(loss_functions_for_training=loss_functions['train'])
+    sufficient_statistics = get_sufficient_statistics(
+        template_for_loss_function=loss_function_for_algorithm, constants=constants)
+    algorithm_for_learning = PacBayesOptimizationAlgorithm(
+        initial_state=initial_state_learned_algorithm,
+        implementation=ConvNet(img_height=image_height, img_width=image_width, kernel_size=3),
+        loss_function=loss_functions['prior'][0],
+        pac_parameters=get_pac_bayes_parameters(sufficient_statistics),
+        constraint=constraint
+    )
+    return algorithm_for_learning
+
+
+def create_parametric_loss_functions_from_parameters(template_loss_function, parameters):
+
+    loss_functions = {
+        name: [ParametricLossFunction(function=template_loss_function, parameter=p)
+               for p in parameters[name]] for name in list(parameters.keys())
+    }
+    return loss_functions
+
+
+def save_data(savings_path, smoothness_parameter, pac_bound, initialization_learned_algorithm,
+              initialization_baseline_algorithm, number_of_iterations, parameters, samples_prior, best_sample):
+
+    np.save(savings_path + 'smoothness_parameter', smoothness_parameter)
+    np.save(savings_path + 'pac_bound', pac_bound)
+    np.save(savings_path + 'initialization_learned_algorithm', initialization_learned_algorithm)
+    np.save(savings_path + 'initialization_baseline_algorithm', initialization_baseline_algorithm)
+    np.save(savings_path + 'number_of_iterations', number_of_iterations)
+    with open(savings_path + 'parameters_problem', 'wb') as file:
+        # noinspection PyTypeChecker
+        pickle.dump(parameters, file)
+
+    parameters_of_estimation = get_parameters_of_estimation()
+    with open(savings_path + 'parameters_of_estimation', 'wb') as file:
+        # noinspection PyTypeChecker
+        pickle.dump(parameters_of_estimation, file)
+    with open(savings_path + 'samples', 'wb') as file:
+        # noinspection PyTypeChecker
+        pickle.dump(samples_prior, file)
+    with open(savings_path + 'best_sample', 'wb') as file:
+        # noinspection PyTypeChecker
+        pickle.dump(best_sample, file)
+
+
+def set_up_and_train_algorithm(path_of_experiment, path_to_images):
+
+    number_of_datapoints_per_dataset = get_number_of_datapoints()
+    parameters, loss_function_of_algorithm, smoothness_parameter = get_data(
+        path_to_images=path_to_images, number_of_datapoints_per_dataset=number_of_datapoints_per_dataset)
+    loss_functions = create_parametric_loss_functions_from_parameters(
+        template_loss_function=loss_function_of_algorithm, parameters=parameters)
+    baseline_algorithm = get_baseline_algorithm(
+        loss_function_of_algorithm=loss_function_of_algorithm, smoothness_parameter=smoothness_parameter)
+    algorithm_for_learning = get_algorithm_for_learning(
+        loss_function_for_algorithm=loss_function_of_algorithm, loss_functions=loss_functions)
+
+    algorithm_for_learning.initialize_with_other_algorithm(other_algorithm=baseline_algorithm,
+                                                           loss_functions=loss_functions['prior'],
+                                                           parameters_of_initialization=get_initialization_parameters())
+
+    fitting_parameters = get_fitting_parameters(maximal_number_of_iterations=algorithm_for_learning.n_max)
+    sampling_parameters = get_sampling_parameters(maximal_number_of_iterations=algorithm_for_learning.n_max)
+    constraint_parameters = get_constraint_parameters(number_of_training_iterations=fitting_parameters['n_max'])
+    update_parameters = get_update_parameters()
+    pac_bound, state_dict_samples_prior = algorithm_for_learning.pac_bayes_fit(
+        loss_functions_prior=loss_functions['prior'],
+        loss_functions_train=loss_functions['train'],
+        fitting_parameters=fitting_parameters,
+        sampling_parameters=sampling_parameters,
+        constraint_parameters=constraint_parameters,
+        update_parameters=update_parameters
+    )
+
+    savings_path = create_folder_for_storing_data(path_of_experiment)
+    save_data(savings_path=savings_path, smoothness_parameter=smoothness_parameter,
+              pac_bound=pac_bound.numpy(),
+              initialization_learned_algorithm=algorithm_for_learning.initial_state.clone().numpy(),
+              initialization_baseline_algorithm=baseline_algorithm.initial_state.clone().numpy(),
+              number_of_iterations=algorithm_for_learning.n_max, parameters=parameters,
+              samples_prior=state_dict_samples_prior, best_sample=algorithm_for_learning.implementation.state_dict())
---
+++ tests/Experiments/CurrentlyWorkingOn/test_training.py
@@ -0,0 +1,224 @@
+import unittest
+from typing import Callable
+import torch
+from algorithms.nesterov_accelerated_gradient_descent import NesterovAcceleratedGradient
+from classes.Constraint.class_Constraint import Constraint
+from classes.LossFunction.class_LossFunction import LossFunction
+from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
+from classes.OptimizationAlgorithm.derived_classes.derived_classes.subclass_PacBayesOptimizationAlgorithm import \
+    PacBayesOptimizationAlgorithm
+from experiments.image_processing.algorithm import ConvNet
+from experiments.image_processing.data_generation import get_data
+from experiments.image_processing.training import (set_up_and_train_algorithm,
+                                                   get_baseline_algorithm,
+                                                   get_initial_states,
+                                                   get_dimension_of_optimization_variable,
+                                                   get_image_height_and_width,
+                                                   create_folder_for_storing_data,
+                                                   get_number_of_datapoints,
+                                                   get_parameters_of_estimation,
+                                                   get_update_parameters,
+                                                   get_sampling_parameters,
+                                                   get_fitting_parameters,
+                                                   get_initialization_parameters,
+                                                   get_describing_property,
+                                                   get_constraint_parameters,
+                                                   get_pac_bayes_parameters,
+                                                   create_parametric_loss_functions_from_parameters,
+                                                   get_constraint,
+                                                   compute_constants_for_sufficient_statistics,
+                                                   get_sufficient_statistics,
+                                                   get_algorithm_for_learning,
+                                                   save_data)
+
+
+class TestTraining(unittest.TestCase):
+
+    def setUp(self):
+        self.path_of_experiment = '/home/michael/Desktop/JMLR_New/Experiments/image_processing'
+        self.image_path = '/home/michael/Desktop/Experiments/Images/'
+        self.dummy_savings_path = self.path_of_experiment + '/dummy_data/'
+
+    def test_get_dimension(self):
+        h, w = get_image_height_and_width()
+        dim = get_dimension_of_optimization_variable()
+        self.assertIsInstance(dim, int)
+        self.assertEqual(dim, h * w)
+
+    def test_initial_states(self):
+        init_base, init_lear = get_initial_states()
+        self.assertIsInstance(init_base, torch.Tensor)
+        self.assertIsInstance(init_lear, torch.Tensor)
+        self.assertTrue(torch.equal(init_base[1:], init_lear))
+
+    def test_get_baseline_algorithm(self):
+
+        def dummy_function(x):
+            return torch.linalg.norm(x) ** 2
+
+        baseline = get_baseline_algorithm(loss_function_of_algorithm=dummy_function, smoothness_parameter=1)
+        self.assertIsInstance(baseline, OptimizationAlgorithm)
+        self.assertIsInstance(baseline.implementation, NesterovAcceleratedGradient)
+
+    def test_create_folder(self):
+        create_folder_for_storing_data(path_of_experiment=self.path_of_experiment)
+
+    def test_get_number_of_datapoints(self):
+        num_data = get_number_of_datapoints()
+        self.assertIsInstance(num_data, dict)
+        self.assertTrue('prior' in num_data.keys())
+        self.assertTrue('train' in num_data.keys())
+        self.assertTrue('test' in num_data.keys())
+        self.assertTrue('validation' in num_data.keys())
+        self.assertTrue(len(num_data.keys()) == 4)
+
+    def test_parameters_of_estimation(self):
+        par = get_parameters_of_estimation()
+        self.assertIsInstance(par, dict)
+        self.assertTrue('quantile_distance' in par.keys())
+        self.assertTrue('quantiles' in par.keys())
+        self.assertTrue('probabilities' in par.keys())
+        self.assertTrue(len(par.keys()) == 3)
+
+    def test_update_parameters(self):
+        par = get_update_parameters()
+        self.assertIsInstance(par, dict)
+        self.assertTrue('num_iter_print_update' in par.keys())
+        self.assertTrue('with_print' in par.keys())
+        self.assertTrue('bins' in par.keys())
+        self.assertTrue(len(par.keys()) == 3)
+
+    def test_sampling_parameters(self):
+        max_number = torch.randint(low=1, high=100, size=(1,)).item()
+        par = get_sampling_parameters(maximal_number_of_iterations=max_number)
+        self.assertIsInstance(par, dict)
+        self.assertTrue('lr' in par.keys())
+        self.assertTrue('length_trajectory' in par.keys())
+        self.assertTrue('with_restarting' in par.keys())
+        self.assertTrue('restart_probability' in par.keys())
+        self.assertTrue('num_samples' in par.keys())
+        self.assertTrue('num_iter_burnin' in par.keys())
+        self.assertTrue(len(par.keys()) == 6)
+        self.assertTrue(par['restart_probability'] == 1/max_number)
+
+    def test_fitting_parameters(self):
+        max_number = torch.randint(low=1, high=100, size=(1,)).item()
+        par = get_fitting_parameters(maximal_number_of_iterations=max_number)
+        self.assertIsInstance(par, dict)
+        self.assertTrue('lr' in par.keys())
+        self.assertTrue('length_trajectory' in par.keys())
+        self.assertTrue('restart_probability' in par.keys())
+        self.assertTrue('n_max' in par.keys())
+        self.assertTrue('num_iter_update_stepsize' in par.keys())
+        self.assertTrue('factor_stepsize_update' in par.keys())
+        self.assertTrue(len(par.keys()) == 6)
+        self.assertTrue(par['restart_probability'] == 1/max_number)
+
+    def test_init_parameters(self):
+        par = get_initialization_parameters()
+        self.assertIsInstance(par, dict)
+        self.assertTrue('lr' in par.keys())
+        self.assertTrue('num_iter_max' in par.keys())
+        self.assertTrue('num_iter_print_update' in par.keys())
+        self.assertTrue('num_iter_update_stepsize' in par.keys())
+        self.assertTrue('with_print' in par.keys())
+        self.assertTrue(len(par.keys()) == 5)
+
+    def test_constraint_parameters(self):
+        max_number = torch.randint(low=1, high=100, size=(1,)).item()
+        par = get_constraint_parameters(number_of_training_iterations=max_number)
+        self.assertIsInstance(par, dict)
+        self.assertTrue('describing_property' in par.keys())
+        self.assertTrue('num_iter_update_constraint' in par.keys())
+        self.assertTrue(len(par.keys()) == 2)
+
+    def test_describing_property(self):
+        reduction_property, convergence_risk_constraint, empirical_second_moment = get_describing_property()
+        self.assertIsInstance(reduction_property, Callable)
+        self.assertIsInstance(convergence_risk_constraint, Callable)
+        self.assertIsInstance(empirical_second_moment, Callable)
+
+    def test_pac_parameters(self):
+        par = get_pac_bayes_parameters(sufficient_statistics=None)  # Dummy
+        self.assertIsInstance(par, dict)
+        self.assertTrue('sufficient_statistics' in par.keys())
+        self.assertTrue('natural_parameters' in par.keys())
+        self.assertTrue('covering_number' in par.keys())
+        self.assertTrue('epsilon' in par.keys())
+        self.assertTrue('n_max' in par.keys())
+        self.assertTrue(len(par.keys()) == 5)
+
+    def test_create_loss_functions_from_parameters(self):
+        number_data = {'prior': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'train': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'test': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'validation': torch.randint(low=1, high=10, size=(1,)).item()}
+        parameters, loss_function_of_algorithm, smoothness = get_data(
+            number_of_datapoints_per_dataset=number_data, path_to_images=self.image_path, device='cpu')
+        loss_functions = create_parametric_loss_functions_from_parameters(
+            template_loss_function=loss_function_of_algorithm, parameters=parameters)
+        self.assertIsInstance(loss_functions, dict)
+        self.assertTrue(len(loss_functions.keys()) == len(number_data.keys()))
+        for name in number_data.keys():
+            self.assertEqual(len(loss_functions[name]), number_data[name])
+
+    def test_get_constraint(self):
+
+        def dummy_function(x, parameter):
+            return parameter['scale'] * torch.linalg.norm(x)
+
+        parameters = {'prior': [{'scale': torch.rand(size=(1,)).item()} for _ in range(0)],
+                      'train': [{'scale': torch.rand(size=(1,)).item()} for _ in range(0)],
+                      'test': [{'scale': torch.rand(size=(1,)).item()} for _ in range(0)],
+                      'validation': [{'scale': torch.rand(size=(1,)).item()} for _ in range(3)]}
+
+        loss_functions = create_parametric_loss_functions_from_parameters(template_loss_function=dummy_function,
+                                                                          parameters=parameters)
+        constraint = get_constraint(loss_functions_for_constraint=loss_functions['validation'])
+        self.assertIsInstance(constraint, Constraint)
+
+    def test_compute_constants_for_sufficient_statistics(self):
+        factor = 0.2
+        exponent = 1.
+        number_of_loss_functions = 10
+        loss_functions = [LossFunction(function=lambda x: (torch.linalg.norm(x)+2)**2)
+                          for _ in range(number_of_loss_functions)]
+        constant = compute_constants_for_sufficient_statistics(loss_functions_for_training=loss_functions)
+        # Note that we have to divide by N (number of loss_functions) twice: one for the computation of the mean, the
+        # other time for the factor 1/N in the PAC-bound. Here, the first 1/N is skipped, as we use N-times the same
+        # function x -> (||x|| + 2)**2. Since the initial state is zero, the norm vanishes, and it remains 2**2 = 4.
+        self.assertTrue(torch.allclose(constant, torch.tensor(factor * 4 ** exponent) ** 2 / len(loss_functions)))
+
+    def test_get_sufficient_statistics(self):
+        sufficient_statistics = get_sufficient_statistics(template_for_loss_function=lambda x: x**2,
+                                                          constants=1)
+        self.assertIsInstance(sufficient_statistics, Callable)
+
+    def test_get_algorithm_for_learning(self):
+        number_data = {'prior': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'train': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'test': torch.randint(low=1, high=10, size=(1,)).item(),
+                       'validation': torch.randint(low=1, high=10, size=(1,)).item()}
+        parameters, loss_function_of_algorithm, smoothness = get_data(
+            number_of_datapoints_per_dataset=number_data, path_to_images=self.image_path, device='cpu')
+        loss_functions = create_parametric_loss_functions_from_parameters(
+            template_loss_function=loss_function_of_algorithm, parameters=parameters)
+        algo = get_algorithm_for_learning(loss_function_for_algorithm=loss_function_of_algorithm,
+                                          loss_functions=loss_functions)
+        self.assertIsInstance(algo, PacBayesOptimizationAlgorithm)
+        self.assertIsInstance(algo.implementation, ConvNet)
+
+    def test_save_data(self):
+        # Dummy data saved into dummy path
+        save_data(savings_path=self.dummy_savings_path,
+                  smoothness_parameter=0.,
+                  pac_bound=0.,
+                  initialization_baseline_algorithm=[],
+                  initialization_learned_algorithm=[],
+                  number_of_iterations=10,
+                  parameters={},
+                  best_sample={},
+                  samples_prior={})
+
+    def test_training(self):
+        set_up_and_train_algorithm(path_to_images=self.image_path, path_of_experiment=self.path_of_experiment)
