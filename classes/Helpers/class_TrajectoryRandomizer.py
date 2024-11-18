2b57622a71c77c6e27afd05e54593332e3c33e50
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 11:16:45 +0100

    Added type-annotations.

--- classes/Helpers/class_TrajectoryRandomizer.py
+++ classes/Helpers/class_TrajectoryRandomizer.py
@@ -9,13 +9,13 @@ class TrajectoryRandomizer:
         self.restart_probability = restart_probability
         self.length_partial_trajectory = length_partial_trajectory
 
-    def get_variable__should_restart(self):
+    def get_variable__should_restart(self) -> bool:
         return self.should_restart
 
-    def set_variable__should_restart__to(self, should_restart: bool):
+    def set_variable__should_restart__to(self, should_restart: bool) -> None:
         if not isinstance(should_restart, bool):
             raise TypeError("Type of 'should_restart' has to be bool.")
         self.should_restart = should_restart
 
-    def get_variable__restart_probability(self):
+    def get_variable__restart_probability(self) -> float:
         return self.restart_probability
