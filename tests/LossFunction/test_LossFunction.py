6544fedb5487f00a1c2922c601ac9ebc71fda596
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 13:51:15 +0100

    Refactoring.

--- tests/LossFunction/test_LossFunction.py
+++ tests/LossFunction/test_LossFunction.py
@@ -3,13 +3,14 @@ import torch
 from classes.LossFunction.class_LossFunction import LossFunction
 
 
-def dummy_function(x):
-    return 0.5 * torch.linalg.norm(x) ** 2
-
-
 class TestLossFunction(unittest.TestCase):
 
     def setUp(self):
+
+        def dummy_function(x):
+            return 0.5 * torch.linalg.norm(x) ** 2
+        self.dummy_function = dummy_function
+
         self.loss_function = LossFunction(function=dummy_function)
 
     def test_creation(self):
@@ -18,7 +19,7 @@ class TestLossFunction(unittest.TestCase):
     def test_call_function(self):
         dimension = torch.randint(low=1, high=50, size=(1,)).item()
         random_point = torch.randn(size=(dimension,))
-        self.assertTrue(torch.equal(self.loss_function(random_point), dummy_function(random_point)))
+        self.assertTrue(torch.equal(self.loss_function(random_point), self.dummy_function(random_point)))
 
     def test_compute_gradient(self):
         dimension = torch.randint(low=1, high=50, size=(1,)).item()
