d2d1f52582f9c30101b6b65ccde191efce121a72
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 11:02:58 +0100

    Added type-annotations.

--- describing_property/reduction_property.py
+++ describing_property/reduction_property.py
@@ -1,41 +1,46 @@
 import torch
 from classes.LossFunction.class_LossFunction import LossFunction
 from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
-from typing import Tuple
+from typing import Tuple, Callable, List
 
 from classes.OptimizationAlgorithm.derived_classes.derived_classes.subclass_PacBayesOptimizationAlgorithm import \
     PacBayesOptimizationAlgorithm
 
 
-def store_current_loss_function_state_and_iteration_counter(optimization_algorithm):
+def store_current_loss_function_state_and_iteration_counter(optimization_algorithm: OptimizationAlgorithm
+                                                            ) -> Tuple[LossFunction, torch.Tensor, int]:
     return (optimization_algorithm.loss_function,
             optimization_algorithm.current_state,
             optimization_algorithm.iteration_counter)
 
 
-def reset_loss_function_state_and_iteration_counter(optimization_algorithm, loss_function, state, iteration_counter):
+def reset_loss_function_state_and_iteration_counter(optimization_algorithm: OptimizationAlgorithm,
+                                                    loss_function: LossFunction,
+                                                    state: torch.Tensor,
+                                                    iteration_counter: int) -> None:
     optimization_algorithm.set_loss_function(loss_function)
     optimization_algorithm.set_current_state(state)
     optimization_algorithm.set_iteration_counter(iteration_counter)
 
 
-def compute_loss_at_beginning_and_end(optimization_algorithm):
+def compute_loss_at_beginning_and_end(optimization_algorithm: OptimizationAlgorithm) -> Tuple[float, float]:
     loss_at_beginning = optimization_algorithm.evaluate_loss_function_at_current_iterate().item()
     _ = [optimization_algorithm.perform_step() for _ in range(optimization_algorithm.n_max)]
     loss_at_end = optimization_algorithm.evaluate_loss_function_at_current_iterate().item()
     return loss_at_beginning, loss_at_end
 
 
-def instantiate_reduction_property_with(factor, exponent):
+def instantiate_reduction_property_with(factor: float, exponent: float) -> Tuple[Callable, Callable, Callable]:
 
-    def convergence_risk_constraint(loss_at_beginning, loss_at_end):
+    def convergence_risk_constraint(loss_at_beginning: float, loss_at_end: float) -> bool:
         return loss_at_end <= factor * loss_at_beginning ** exponent
 
-    def empirical_second_moment(list_of_loss_functions, point):
+    def empirical_second_moment(list_of_loss_functions: List[LossFunction], point: torch.Tensor) -> torch.Tensor:
         return torch.mean(torch.stack([(factor * loss_function(point) ** exponent) ** 2
                                        for loss_function in list_of_loss_functions]))
 
-    def reduction_property(loss_function_to_test, optimization_algorithm: PacBayesOptimizationAlgorithm):
+    def reduction_property(loss_function_to_test: LossFunction,
+                           optimization_algorithm: PacBayesOptimizationAlgorithm) -> bool:
 
         current_loss_function, current_state, current_iteration_counter = (
             store_current_loss_function_state_and_iteration_counter(optimization_algorithm))
