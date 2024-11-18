518b50f0cb411692cef0870c6b5f1b802fd0c3f4
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 09:57:28 +0100

    Added type-annotations.

--- experiments/quadratics/run_experiment.py
+++ experiments/quadratics/run_experiment.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import torch
 
 
-def create_folder_for_experiment(path_to_experiment_folder):
+def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
     path_of_experiment = path_to_experiment_folder + "/quadratics/"
     Path(path_of_experiment).mkdir(parents=True, exist_ok=True)
     return path_of_experiment
 
 
-def run(path_to_experiment_folder):
+def run(path_to_experiment_folder: str) -> None:
     # This is pretty important! Without increased accuracy, the model will struggle to train, because at some point
     # (about loss of 1e-6) the incurred losses are subject to numerical instabilities, which do not provide meaningful
     # information for learning.
