ec66f7ba61bfa870c88b59a87c70a39359c1a568
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 16:09:45 +0100

    Refactoring.

--- tests/SufficientStatistics/test_sufficient_statistics.py
+++ tests/SufficientStatistics/test_sufficient_statistics.py
@@ -5,16 +5,16 @@ from classes.LossFunction.derived_classes.subclass_ParametricLossFunction import
 from classes.OptimizationAlgorithm.derived_classes.derived_classes.subclass_PacBayesOptimizationAlgorithm import (
     PacBayesOptimizationAlgorithm)
 from algorithms.dummy import Dummy
-from describing_property.reduction_property import instantiate_reduction_property_with
-
-
-def dummy_function(x, parameter=None):
-    return 0.5 * torch.linalg.norm(x) ** 2
 
 
 class TestSufficientStatistics(unittest.TestCase):
 
     def setUp(self):
+
+        def dummy_function(x, parameter):
+            return parameter['p'] * torch.linalg.norm(x) ** 2
+
+        self.dummy_function = dummy_function
         dim = torch.randint(low=1, high=1000, size=(1,)).item()
         length_state = 1  # Take one, because it has to be compatible with Dummy()
         self.initial_state = torch.randn(size=(length_state, dim))
@@ -38,12 +38,13 @@ class TestSufficientStatistics(unittest.TestCase):
 
         values = evaluate_sufficient_statistics(
             optimization_algorithm=self.pac_algorithm,
-            parameter_of_loss_function={'p': 1},
-            template_for_loss_function=dummy_function,
-            constants=1,
+            loss_function=ParametricLossFunction(function=self.dummy_function, parameter={'p': 1}),
+            constants=torch.tensor(1),
             convergence_risk_constraint=dummy_constraint,
-            convergence_probability=1.
+            convergence_probability=torch.tensor(1.)
         )
+
+        # In case the constraint is not satisfied, the function should return [0, 0].
         self.assertTrue(torch.equal(values, torch.tensor([0.0, 0.0])))
 
     def test_constraint_satisfied(self):
@@ -51,17 +52,18 @@ class TestSufficientStatistics(unittest.TestCase):
         def dummy_constraint(loss_at_beginning, loss_at_end):
             return True
 
-        constants = torch.randint(low=1, high=1000, size=(1,)).item()
-        convergence_probability = torch.rand((1,)).item()
+        constants = torch.randint(low=1, high=1000, size=(1,))
+        convergence_probability = torch.rand((1,))
         values = evaluate_sufficient_statistics(
             optimization_algorithm=self.pac_algorithm,
-            parameter_of_loss_function={'p': 1},
-            template_for_loss_function=dummy_function,
+            loss_function=ParametricLossFunction(function=self.dummy_function, parameter={'p': 1}),
             constants=constants,
             convergence_risk_constraint=dummy_constraint,
             convergence_probability=convergence_probability
         )
         loss_at_end = self.pac_algorithm.loss_function(self.pac_algorithm.current_iterate)
+
+        # In case the constraint is satisfied, the function should actually compute the values.
         self.assertTrue(torch.equal(
             values, torch.tensor([-loss_at_end/convergence_probability, constants / (convergence_probability**2)])
         ))
