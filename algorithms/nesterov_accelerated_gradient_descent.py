b4182a91c416ea3cb05ccc5381227f5040c2730d
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 11:26:26 +0100

    Added type-annotations.

--- algorithms/nesterov_accelerated_gradient_descent.py
+++ algorithms/nesterov_accelerated_gradient_descent.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
+
 
 class NesterovAcceleratedGradient(nn.Module):
 
@@ -8,7 +10,7 @@ class NesterovAcceleratedGradient(nn.Module):
         super(NesterovAcceleratedGradient, self).__init__()
         self.alpha = nn.Parameter(alpha)
 
-    def forward(self, opt_algo):
+    def forward(self, opt_algo: OptimizationAlgorithm) -> torch.Tensor:
         t_new = 0.5 * (1.0 + torch.sqrt(1.0 + 4 * opt_algo.current_state[0][-1].clone() ** 2))
         y_k = (opt_algo.current_state[2] + ((opt_algo.current_state[0][-1] - 1) / t_new) *
                (opt_algo.current_state[2] - opt_algo.current_state[1]))
@@ -18,7 +20,7 @@ class NesterovAcceleratedGradient(nn.Module):
         return result
 
     @staticmethod
-    def update_state(opt_algo):
+    def update_state(opt_algo: OptimizationAlgorithm) -> None:
         opt_algo.current_state[0][-1] = 0.5 * (1.0 + torch.sqrt(1.0 + 4 * opt_algo.current_state[0][-1].clone() ** 2))
         opt_algo.current_state[1] = opt_algo.current_state[2].detach().clone()
         opt_algo.current_state[2] = opt_algo.current_iterate.detach().clone()
