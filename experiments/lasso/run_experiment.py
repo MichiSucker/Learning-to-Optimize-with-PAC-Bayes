ed5d545cc8e65dc976cae82a59989368621d6965
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 09:11:08 +0100

    Adjusted accuracy of default-tensor and added type-annotations.

--- experiments/lasso/run_experiment.py
+++ experiments/lasso/run_experiment.py
@@ -2,15 +2,18 @@ from experiments.lasso.training import set_up_and_train_algorithm
 from experiments.lasso.evaluation import evaluate_algorithm
 from experiments.lasso.plotting import create_evaluation_plots
 from pathlib import Path
+import torch
 
 
-def create_folder_for_experiment(path_to_experiment_folder):
+def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
     path_of_experiment = path_to_experiment_folder + "/lasso/"
     Path(path_of_experiment).mkdir(parents=True, exist_ok=True)
     return path_of_experiment
 
 
-def run(path_to_experiment_folder):
+def run(path_to_experiment_folder: str) -> None:
+    # This is pretty important again. Also, it makes sure that all tensor types do match.
+    torch.set_default_dtype(torch.float64)
     path_of_experiment = create_folder_for_experiment(path_to_experiment_folder)
     set_up_and_train_algorithm(path_of_experiment=path_of_experiment)
     evaluate_algorithm(path_of_experiment=path_of_experiment, loading_path=path_of_experiment + 'data/')
