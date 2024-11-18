dd076c13575056388c363bf08a7385dc2b41dbf0
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 11:17:31 +0100

    Added type-annotations.

--- classes/Constraint/class_Constraint.py
+++ classes/Constraint/class_Constraint.py
@@ -1,4 +1,6 @@
-from typing import Callable
+from typing import Callable, List
+
+from classes.LossFunction.class_LossFunction import LossFunction
 from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
 
 
@@ -11,7 +13,7 @@ class Constraint:
         return self.function(optimization_algorithm, *args, **kwargs)
 
 
-def create_list_of_constraints_from_functions(describing_property, list_of_functions):
+def create_list_of_constraints_from_functions(describing_property: Callable, list_of_functions: List[LossFunction]):
     list_of_constraints = [
         Constraint(function=lambda opt_algo, i=i: describing_property(list_of_functions[i], opt_algo))
         for i in range(len(list_of_functions))]
