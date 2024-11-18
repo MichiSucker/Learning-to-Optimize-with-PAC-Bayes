9db7c066d17e4650d12b88101f0de17a78d7e820
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 10:53:30 +0100

    Added type-annotations.

--- experiments/image_processing/run_experiment.py
+++ experiments/image_processing/run_experiment.py
@@ -4,13 +4,13 @@ from experiments.image_processing.plotting import create_evaluation_plots
 from pathlib import Path
 
 
-def create_folder_for_experiment(path_to_experiment_folder):
+def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
     path_of_experiment = path_to_experiment_folder + "/image_processing/"
     Path(path_of_experiment).mkdir(parents=True, exist_ok=True)
     return path_of_experiment
 
 
-def run(path_to_experiment_folder):
+def run(path_to_experiment_folder: str) -> None:
     path_to_images = '/home/michael/Desktop/Experiments/Images/'
     path_of_experiment = create_folder_for_experiment(path_to_experiment_folder)
     set_up_and_train_algorithm(path_of_experiment=path_of_experiment, path_to_images=path_to_images)
