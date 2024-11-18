7c527eb5baa00db3c98f309d1d5ca9452a05cec1
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 10:23:25 +0100

    Added type-annotations.

--- experiments/nn_training/run_experiment.py
+++ experiments/nn_training/run_experiment.py
@@ -4,13 +4,13 @@ from experiments.nn_training.plotting import create_evaluation_plots
 from pathlib import Path
 
 
-def create_folder_for_experiment(path_to_experiment_folder):
+def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
     path_of_experiment = path_to_experiment_folder + "/neural_network_training/"
     Path(path_of_experiment).mkdir(parents=True, exist_ok=True)
     return path_of_experiment
 
 
-def run(path_to_experiment_folder):
+def run(path_to_experiment_folder: str) -> None:
 
     path_of_experiment = create_folder_for_experiment(path_to_experiment_folder)
     set_up_and_train_algorithm(path_of_experiment=path_of_experiment)
