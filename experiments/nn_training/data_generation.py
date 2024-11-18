897a36a987b9041b44642254b4ee3e10eebb08c5
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 10:11:55 +0100

    Added type-annotations.

--- experiments/nn_training/data_generation.py
+++ experiments/nn_training/data_generation.py
@@ -1,43 +1,50 @@
 import torch
 import torch.nn as nn
+from typing import Callable, Tuple
+from experiments.nn_training.neural_network import NeuralNetworkForLearning
 
 
-def get_loss_of_neural_network():
+def get_loss_of_neural_network() -> Callable:
     return nn.MSELoss()
 
 
-def get_distribution_of_datapoints():
+def get_distribution_of_datapoints() -> torch.distributions.Distribution:
     return torch.distributions.uniform.Uniform(-2., 2.)
 
 
-def get_distribution_of_coefficients():
+def get_distribution_of_coefficients() -> torch.distributions.Distribution:
     return torch.distributions.uniform.Uniform(-5, 5)
 
 
-def get_powers_of_polynomials():
+def get_powers_of_polynomials() -> torch.Tensor:
     # Note that we return 6 here, because then torch.arange will return the numbers [0, ..., 5].
     return torch.arange(5+1)
 
 
-def get_observations_for_x_values(number_of_samples, distribution_x_values):
+def get_observations_for_x_values(number_of_samples: int,
+                                  distribution_x_values: torch.distributions.Distribution) -> torch.Tensor:
     xes = distribution_x_values.sample(torch.Size((number_of_samples,)))
     xes, _ = torch.sort(xes)  # Sort them already now. This is needed, at least, for plotting.
     return xes.reshape((-1, 1))
 
 
-def get_coefficients(distribution_of_coefficients, maximal_degree):
+def get_coefficients(distribution_of_coefficients: torch.distributions.Distribution,
+                     maximal_degree: int) -> torch.Tensor:
     return distribution_of_coefficients.sample(torch.Size((maximal_degree + 1, )))
 
 
-def get_ground_truth_values(x_values, coefficients, powers):
+def get_ground_truth_values(x_values: torch.Tensor, coefficients: torch.Tensor, powers: torch.Tensor) -> torch.Tensor:
     return torch.sum(coefficients * (x_values ** powers), dim=1).reshape((-1, 1))
 
 
-def get_y_values(ground_truth):
+def get_y_values(ground_truth: torch.Tensor) -> torch.Tensor:
     return ground_truth + torch.randn(ground_truth.shape)
 
 
-def create_parameter(x_values, y_values, ground_truth_values, coefficients):
+def create_parameter(x_values: torch.Tensor,
+                     y_values: torch.Tensor,
+                     ground_truth_values: torch.Tensor,
+                     coefficients: torch.Tensor) -> dict:
     return {'x_values': x_values,
             'y_values': y_values,
             'ground_truth_values': ground_truth_values,
@@ -45,7 +52,7 @@ def create_parameter(x_values, y_values, ground_truth_values, coefficients):
             'optimal_loss': torch.tensor(0.0)}
 
 
-def check_and_extract_number_of_datapoints(number_of_datapoints_per_dataset):
+def check_and_extract_number_of_datapoints(number_of_datapoints_per_dataset: dict) -> Tuple[int, int, int, int]:
     if (('prior' not in number_of_datapoints_per_dataset)
             or ('train' not in number_of_datapoints_per_dataset)
             or ('test' not in number_of_datapoints_per_dataset)
@@ -58,7 +65,7 @@ def check_and_extract_number_of_datapoints(number_of_datapoints_per_dataset):
                 number_of_datapoints_per_dataset['validation'])
 
 
-def get_loss_of_algorithm(neural_network, loss_of_neural_network):
+def get_loss_of_algorithm(neural_network: NeuralNetworkForLearning, loss_of_neural_network: Callable):
 
     def loss_function_of_the_algorithm(x: torch.Tensor, parameter: dict) -> torch.Tensor:
         return loss_of_neural_network(neural_network(x=parameter['x_values'], neural_net_parameters=x),
@@ -67,7 +74,7 @@ def get_loss_of_algorithm(neural_network, loss_of_neural_network):
     return loss_function_of_the_algorithm
 
 
-def get_parameters(number_of_datapoints_per_dataset):
+def get_parameters(number_of_datapoints_per_dataset: dict) -> dict:
 
     n_prior, n_train, n_test, n_val = check_and_extract_number_of_datapoints(number_of_datapoints_per_dataset)
     distribution_of_datapoints = get_distribution_of_datapoints()
@@ -80,7 +87,7 @@ def get_parameters(number_of_datapoints_per_dataset):
             x_values = get_observations_for_x_values(number_of_samples=50,
                                                      distribution_x_values=distribution_of_datapoints)
             coefficients = get_coefficients(distribution_of_coefficients,
-                                            maximal_degree=torch.max(powers_of_polynomials))
+                                            maximal_degree=torch.max(powers_of_polynomials).item())
             ground_truth_values = get_ground_truth_values(x_values=x_values, coefficients=coefficients,
                                                           powers=powers_of_polynomials)
             y_values = get_y_values(ground_truth_values)
@@ -90,7 +97,8 @@ def get_parameters(number_of_datapoints_per_dataset):
     return parameters
 
 
-def get_data(neural_network, number_of_datapoints_per_dataset):
+def get_data(neural_network: NeuralNetworkForLearning,
+             number_of_datapoints_per_dataset: dict) -> Tuple[Callable, Callable, dict]:
 
     parameters = get_parameters(number_of_datapoints_per_dataset)
     loss_of_neural_network = get_loss_of_neural_network()
