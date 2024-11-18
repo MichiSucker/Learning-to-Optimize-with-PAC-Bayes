bf3221c7fd53495cbee65a160c45d2363986fdd3
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 14:12:54 +0100

    Refactoring.

--- tests/ParametricOptimizationAlgorithm/test_ParametricOptimizationAlgorithm.py
+++ tests/ParametricOptimizationAlgorithm/test_ParametricOptimizationAlgorithm.py
@@ -7,13 +7,12 @@ from algorithms.dummy import Dummy
 import copy
 
 
-def dummy_function(x):
-    return 0.5 * torch.linalg.norm(x) ** 2
-
-
 class TestParametricOptimizationAlgorithm(unittest.TestCase):
 
     def setUp(self):
+        def dummy_function(x):
+            return 0.5 * torch.linalg.norm(x) ** 2
+
         self.dim = torch.randint(low=1, high=1000, size=(1,)).item()
         self.length_state = 1  # Take one, because it has to be compatible with Dummy()
         self.initial_state = torch.randn(size=(self.length_state, self.dim))
