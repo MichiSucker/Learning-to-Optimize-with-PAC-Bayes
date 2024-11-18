bbb3c7426c33117e455ed35bcaaf571c54c05b27
Michael Sucker <michael_sucker@t-online.de>
2024-11-18 11:15:28 +0100

    Added type-annotations.

--- classes/Helpers/class_SamplingAssistant.py
+++ classes/Helpers/class_SamplingAssistant.py
@@ -1,10 +1,16 @@
 import copy
+from typing import Tuple, List
 from tqdm import tqdm
+import torch.nn as nn
+from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
 
 
 class SamplingAssistant:
 
-    def __init__(self, learning_rate, desired_number_of_samples, number_of_iterations_burnin):
+    def __init__(self,
+                 learning_rate: float,
+                 desired_number_of_samples: int,
+                 number_of_iterations_burnin: int):
         self.initial_learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.number_of_correct_samples = 0
@@ -18,36 +24,36 @@ class SamplingAssistant:
         self.progressbar = tqdm(total=self.desired_number_of_samples + self.number_of_iterations_burnin)
         self.progressbar.set_description('Sampling')
 
-    def decay_learning_rate(self, iteration):
+    def decay_learning_rate(self, iteration: int) -> None:
         self.current_learning_rate = self.initial_learning_rate / iteration
 
-    def set_point_that_satisfies_constraint(self, state_dict):
+    def set_point_that_satisfies_constraint(self, state_dict: dict) -> None:
         if not isinstance(state_dict, dict):
             raise TypeError("Provided point is not a dict (in particular not a state_dict()).")
         self.point_that_satisfies_constraint = copy.deepcopy(state_dict)
 
-    def set_noise_distributions(self, noise_distributions):
+    def set_noise_distributions(self, noise_distributions: dict) -> None:
         if not isinstance(noise_distributions, dict):
             raise TypeError("Provided input is not a dict.")
         self.noise_distributions = noise_distributions
 
-    def should_continue(self):
+    def should_continue(self) -> bool:
         return self.number_of_correct_samples < self.desired_number_of_samples + self.number_of_iterations_burnin
 
-    def should_store_sample(self, iteration):
+    def should_store_sample(self, iteration: int) -> bool:
         return iteration >= self.number_of_iterations_burnin
 
-    def reject_sample(self, optimization_algorithm):
+    def reject_sample(self, optimization_algorithm: OptimizationAlgorithm) -> None:
         optimization_algorithm.implementation.load_state_dict(self.point_that_satisfies_constraint)
 
-    def store_sample(self, implementation, estimated_probability):
+    def store_sample(self, implementation: nn.Module, estimated_probability: float) -> None:
         self.samples.append([p.detach().clone() for p in implementation.parameters() if p.requires_grad])
         self.samples_state_dict.append(copy.deepcopy(implementation.state_dict()))
         self.estimated_probabilities.append(estimated_probability)
         self.number_of_correct_samples += 1
         self.progressbar.update(1)
 
-    def prepare_output(self):
+    def prepare_output(self) -> Tuple[List, List, List]:
         if any((len(self.samples) < self.desired_number_of_samples,
                 len(self.samples_state_dict) < self.desired_number_of_samples,
                 len(self.estimated_probabilities) < self.desired_number_of_samples)):
