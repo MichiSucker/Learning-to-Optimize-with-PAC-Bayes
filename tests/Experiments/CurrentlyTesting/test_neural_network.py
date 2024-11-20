60913f199201cc9b4a458b6a939fcaf61799c8dd
Michael Sucker <michael_sucker@t-online.de>
2024-11-20 09:11:57 +0100

    Refactoring.

--- tests/Experiments/CurrentlyTesting/test_neural_network.py
+++ tests/Experiments/CurrentlyTesting/test_neural_network.py
@@ -10,6 +10,8 @@ from experiments.nn_training.data_generation import get_data
 class TestNeuralNetwork(unittest.TestCase):
 
     def test_polynomial_features(self):
+        # Check that the polynomial features are computed as specified below, that is, [x**1, x**2, ...].
+        # And check that it also works correctly for whole data sets.
         degree = torch.randint(low=1, high=10, size=(1,)).item()
         x = torch.randn(size=(1,))
         pol = polynomial_features(x=x, degree=degree)
@@ -25,6 +27,7 @@ class TestNeuralNetwork(unittest.TestCase):
             self.assertTrue(torch.allclose(pol[i], (x[i] ** torch.arange(1, degree+1)).reshape((1, -1))))
 
     def test_get_shape_parameters(self):
+        # Check that we get a list with one entry for each parameter of the neural network.
         degree = 5
         nn_std = NeuralNetworkForStandardTraining(degree=degree)
         shape_parameters = nn_std.get_shape_parameters()
@@ -32,21 +35,16 @@ class TestNeuralNetwork(unittest.TestCase):
         self.assertEqual(shape_parameters, [p.shape for p in nn_std.parameters()])
 
     def test_get_dimension_of_hyperparameters(self):
+        # Check that the dimension of the hyperparameters is the sum of the single dimensions of the parameters of
+        # the neural network.
         degree = 5
         nn_std = NeuralNetworkForStandardTraining(degree=degree)
         dim = nn_std.get_dimension_of_hyperparameters()
         self.assertIsInstance(dim, int)
         self.assertEqual(dim, sum([torch.prod(torch.tensor(p.shape)).item() for p in nn_std.parameters()]))
 
-    def test_nn_to_tensor(self):
-        degree = 5
-        nn_std = NeuralNetworkForStandardTraining(degree=degree)
-        dim = nn_std.get_dimension_of_hyperparameters()
-        all_parameters = nn_std.transform_parameters_to_tensor()
-        self.assertIsInstance(all_parameters, torch.Tensor)
-        self.assertEqual(len(all_parameters), dim)
-
     def test_load_tensor_as_parameters_of_nn(self):
+        # Check that we can load parameters into the neural network, that is, set specific weights.
         degree = 5
         nn_std = NeuralNetworkForStandardTraining(degree=degree)
         all_parameters = nn_std.transform_parameters_to_tensor()
@@ -56,7 +54,18 @@ class TestNeuralNetwork(unittest.TestCase):
         parameters_now = nn_std.transform_parameters_to_tensor()
         self.assertTrue(torch.equal(parameters_now, random_parameters))
 
+    def test_nn_to_tensor(self):
+        # Check that we can flatten the weights of the neural network into a big tensor.
+        degree = 5
+        nn_std = NeuralNetworkForStandardTraining(degree=degree)
+        dim = nn_std.get_dimension_of_hyperparameters()
+        all_parameters = nn_std.transform_parameters_to_tensor()
+        self.assertIsInstance(all_parameters, torch.Tensor)
+        self.assertEqual(len(all_parameters), dim)
+
     def test_call_and_compare_neural_networks(self):
+        # Call the neural network one time in the usual way, and one time in the way that is needed for learning
+        # (weights are not the leafs in the computational graph), and make sure that the result do match-up.
         degree = 5
         nn_std = NeuralNetworkForStandardTraining(degree=degree)
         x = torch.randn(size=(10,))
@@ -72,6 +81,8 @@ class TestNeuralNetwork(unittest.TestCase):
         self.assertTrue(torch.equal(nn_std(x), nn_for_learning(x, parameters_of_neural_network)))
 
     def test_train_model(self):
+        # Weak test: Check that training changes the weights, and that the loss decreases compared to the beginning.
+        # Also: Check that each iterate is a hyperparameter.
         degree = 5
         nn_std = NeuralNetworkForStandardTraining(degree=degree)
         shape_parameters = nn_std.get_shape_parameters()
