82172623be9d2866fe3526db6ce1382dee39ab89
Michael Sucker <michael_sucker@t-online.de>
2024-11-20 09:37:10 +0100

    Refactoring + more tests.

--- tests/Experiments/CurrentlyTesting/test_training.py
+++ tests/Experiments/CurrentlyTesting/test_training.py
@@ -1,8 +1,7 @@
 import unittest
 from typing import Callable
-
+import numpy as np
 import torch
-from main import TESTING_LEVEL
 from algorithms.gradient_descent import GradientDescent
 from classes.Constraint.class_Constraint import Constraint
 from classes.LossFunction.class_LossFunction import LossFunction
@@ -10,7 +9,6 @@ from classes.LossFunction.derived_classes.subclass_ParametricLossFunction import
 from classes.OptimizationAlgorithm.class_OptimizationAlgorithm import OptimizationAlgorithm
 from classes.OptimizationAlgorithm.derived_classes.derived_classes.subclass_PacBayesOptimizationAlgorithm import \
     PacBayesOptimizationAlgorithm
-from exponential_family.natural_parameters.natural_parameters import evaluate_natural_parameters_at
 from experiments.nn_training.training import (instantiate_neural_networks,
                                               create_parametric_loss_functions_from_parameters,
                                               get_algorithm_for_initialization,
@@ -28,20 +26,111 @@ from experiments.nn_training.training import (instantiate_neural_networks,
                                               get_initialization_parameters,
                                               get_constraint_parameters,
                                               get_number_of_datapoints,
-                                              set_up_and_train_algorithm)
+                                              set_up_and_train_algorithm,
+                                              create_folder_for_storing_data,
+                                              save_data)
 from experiments.nn_training.neural_network import NeuralNetworkForStandardTraining, NeuralNetworkForLearning
 
 
 class TestEvaluationNN(unittest.TestCase):
 
+    def test_get_number_of_datapoints(self):
+        # Check that each data set is specified
+        number_of_datapoints = get_number_of_datapoints()
+        self.assertTrue('prior' in number_of_datapoints.keys())
+        self.assertTrue('train' in number_of_datapoints.keys())
+        self.assertTrue('test' in number_of_datapoints.keys())
+        self.assertTrue('validation' in number_of_datapoints.keys())
+        self.assertTrue(len(number_of_datapoints.keys()) == 4)
+
+    def test_get_initialization_parameters(self):
+        # Check that each data set is specified, and only those.
+        initialization_parameters = get_initialization_parameters()
+        self.assertTrue('lr' in initialization_parameters.keys())
+        self.assertTrue('num_iter_max' in initialization_parameters.keys())
+        self.assertTrue('num_iter_print_update' in initialization_parameters.keys())
+        self.assertTrue('num_iter_update_stepsize' in initialization_parameters.keys())
+        self.assertTrue('with_print' in initialization_parameters.keys())
+        self.assertTrue(len(initialization_parameters.keys()) == 5)
+
+    def test_get_fitting_parameters(self):
+        # Check that each data set is specified, and only those.
+        # Also check that restart_probability is specified correctly.
+        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
+        fitting_parameters = get_fitting_parameters(maximal_number_of_iterations=maximal_number_of_iterations)
+        self.assertTrue('restart_probability' in fitting_parameters.keys())
+        self.assertTrue('length_trajectory' in fitting_parameters.keys())
+        self.assertTrue('n_max' in fitting_parameters.keys())
+        self.assertTrue('lr' in fitting_parameters.keys())
+        self.assertTrue('num_iter_update_stepsize' in fitting_parameters.keys())
+        self.assertTrue('factor_stepsize_update' in fitting_parameters.keys())
+        self.assertTrue(len(fitting_parameters.keys()) == 6)
+        self.assertTrue(fitting_parameters['restart_probability']
+                        == fitting_parameters['length_trajectory']/maximal_number_of_iterations)
+
+    def test_get_sampling_parameters(self):
+        # Check that each data set is specified, and only those.
+        # Also check that restart_probability is specified correctly.
+        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
+        sampling_parameters = get_sampling_parameters(maximal_number_of_iterations)
+        self.assertTrue('restart_probability' in sampling_parameters.keys())
+        self.assertTrue('length_trajectory' in sampling_parameters.keys())
+        self.assertTrue('num_samples' in sampling_parameters.keys())
+        self.assertTrue('lr' in sampling_parameters.keys())
+        self.assertTrue('with_restarting' in sampling_parameters.keys())
+        self.assertTrue('num_iter_burnin' in sampling_parameters.keys())
+        self.assertTrue(len(sampling_parameters.keys()) == 6)
+        self.assertTrue(sampling_parameters['restart_probability']
+                        == sampling_parameters['length_trajectory']/maximal_number_of_iterations)
+
+    def test_get_update_parameters(self):
+        # Check that each data set is specified, and only those.
+        update_parameters = get_update_parameters()
+        self.assertTrue('num_iter_print_update' in update_parameters.keys())
+        self.assertTrue('with_print' in update_parameters.keys())
+        self.assertTrue('bins' in update_parameters.keys())
+        self.assertTrue(len(update_parameters.keys()) == 3)
+
+    def test_get_parameters_of_estimation(self):
+        # Check that each data set is specified, and only those.
+        estimation_parameters = get_parameters_of_estimation()
+        self.assertTrue('quantile_distance' in estimation_parameters.keys())
+        self.assertTrue('quantiles' in estimation_parameters.keys())
+        self.assertTrue('probabilities' in estimation_parameters.keys())
+        self.assertTrue(len(estimation_parameters.keys()) == 3)
+
+    def test_get_constraint_parameters(self):
+        # Check that each data set is specified, and only those.
+        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
+        constraint_parameters = get_constraint_parameters(maximal_number_of_iterations)
+        self.assertTrue('describing_property' in constraint_parameters.keys())
+        self.assertTrue('num_iter_update_constraint' in constraint_parameters.keys())
+        self.assertTrue(len(constraint_parameters.keys()) == 2)
+
+    def test_get_pac_bayes_parameters(self):
+
+        def dummy_statistics(x):
+            return x
+
+        # Check that each data set is specified, and only those.
+        pac_bayes_parameters = get_pac_bayes_parameters(sufficient_statistics=dummy_statistics)
+        self.assertTrue('sufficient_statistics' in pac_bayes_parameters.keys())
+        self.assertTrue('natural_parameters' in pac_bayes_parameters.keys())
+        self.assertTrue('covering_number' in pac_bayes_parameters.keys())
+        self.assertTrue('epsilon' in pac_bayes_parameters.keys())
+        self.assertTrue('n_max' in pac_bayes_parameters.keys())
+        self.assertTrue(len(pac_bayes_parameters.keys()) == 5)
+
     def test_instantiate_neural_networks(self):
+        # Check that we get both version of the neural network.
         nn_std, nn_learn = instantiate_neural_networks()
         self.assertIsInstance(nn_std, NeuralNetworkForStandardTraining)
         self.assertIsInstance(nn_learn, NeuralNetworkForLearning)
         self.assertEqual(nn_learn.degree, 5)
 
-    def test_instantiate_parametric_loss_functions(self):
+    def test_create_parametric_loss_functions(self):
 
+        # Initialize setting.
         def dummy_function(x, parameter):
             return parameter['scale'] * torch.linalg.norm(x)
 
@@ -54,6 +143,8 @@ class TestEvaluationNN(unittest.TestCase):
                       'test': [{'scale': torch.rand(size=(1,)).item()} for _ in range(n_test)],
                       'validation': [{'scale': torch.rand(size=(1,)).item()} for _ in range(n_val)]}
 
+        # Check that we get a dictionary with four entries, where each entry corresponds to one data set and is a list
+        # of ParametricLossFunctions.
         loss_functions = create_parametric_loss_functions_from_parameters(template_loss_function=dummy_function,
                                                                           parameters=parameters)
         self.assertIsInstance(loss_functions, dict)
@@ -61,24 +152,18 @@ class TestEvaluationNN(unittest.TestCase):
         self.assertEqual(len(loss_functions['train']), n_train)
         self.assertEqual(len(loss_functions['test']), n_test)
         self.assertEqual(len(loss_functions['validation']), n_val)
+        self.assertTrue(len(loss_functions.keys()) == 4)
 
         for name in ['prior', 'train', 'test', 'validation']:
             for function in loss_functions[name]:
                 self.assertIsInstance(function, ParametricLossFunction)
-
-    def test_get_initial_state(self):
-        dim = torch.randint(low=1, high=100, size=(1,)).item()
-        x_0 = get_initial_state(dim=dim)
-        self.assertEqual(x_0.shape, torch.Size((2, dim)))
-        for _ in range(3):
-            # Make sure, you get same init every time.
-            self.assertTrue(torch.equal(x_0, get_initial_state(dim=dim)))
 
     def test_get_algorithm_for_initialization(self):
 
         def dummy_function(x):
             return torch.linalg.norm(x)
 
+        # Check that we initialize with GradientDescent.
         dim = torch.randint(low=1, high=100, size=(1,)).item()
         x_0 = get_initial_state(dim=dim)
         algo = get_algorithm_for_initialization(initial_state_for_std_algorithm=x_0[-1].reshape((1, -1)),
@@ -86,6 +171,24 @@ class TestEvaluationNN(unittest.TestCase):
         self.assertIsInstance(algo, OptimizationAlgorithm)
         self.assertIsInstance(algo.implementation, GradientDescent)
         self.assertEqual(algo.initial_state.shape, torch.Size((1, dim)))
+
+    def test_get_initial_state(self):
+        dim = torch.randint(low=1, high=100, size=(1,)).item()
+        x_0 = get_initial_state(dim=dim)
+        self.assertEqual(x_0.shape, torch.Size((2, dim)))
+        for _ in range(3):
+            # Make sure, you get same init every time.
+            # It is randomly sampled, but always the same.
+            # Note that we need random numbers, because these are the weights of the neural network, and initialization
+            # is important here.
+            self.assertTrue(torch.equal(x_0, get_initial_state(dim=dim)))
+
+    def test_get_describing_property(self):
+        # Check that you get three functions.
+        reduction_property, convergence_risk_constraint, empirical_second_moment = get_describing_property()
+        self.assertIsInstance(reduction_property, Callable)
+        self.assertIsInstance(convergence_risk_constraint, Callable)
+        self.assertIsInstance(empirical_second_moment, Callable)
 
     def test_get_constraint(self):
 
@@ -97,6 +200,7 @@ class TestEvaluationNN(unittest.TestCase):
                       'test': [{'scale': torch.rand(size=(1,)).item()} for _ in range(0)],
                       'validation': [{'scale': torch.rand(size=(1,)).item()} for _ in range(3)]}
 
+        # Check that you get a Constraint-object.
         loss_functions = create_parametric_loss_functions_from_parameters(template_loss_function=dummy_function,
                                                                           parameters=parameters)
         constraint = get_constraint(parameters_of_estimation={'quantile_distance': 0.1,
@@ -104,12 +208,6 @@ class TestEvaluationNN(unittest.TestCase):
                                                               'probabilities': (0.9, 1.0)},
                                     loss_functions_for_constraint=loss_functions['validation'])
         self.assertIsInstance(constraint, Constraint)
-
-    def test_get_describing_property(self):
-        reduction_property, convergence_risk_constraint, empirical_second_moment = get_describing_property()
-        self.assertIsInstance(reduction_property, Callable)
-        self.assertIsInstance(convergence_risk_constraint, Callable)
-        self.assertIsInstance(empirical_second_moment, Callable)
 
     def test_compute_constants_for_sufficient_statistics(self):
         factor = 0.25
@@ -127,8 +225,8 @@ class TestEvaluationNN(unittest.TestCase):
                            (factor * (initial_state[-1].flatten() ** 2) ** exponent) ** 2 / len(loss_functions)))
 
     def test_get_sufficient_statistics(self):
-        sufficient_statistics = get_sufficient_statistics(template_for_loss_function=lambda x: x**2,
-                                                          constants=1)
+        # Check that you get a callable.
+        sufficient_statistics = get_sufficient_statistics(constants=torch.tensor(1.))
         self.assertIsInstance(sufficient_statistics, Callable)
 
     def test_instantiate_algorithm_for_learning(self):
@@ -147,9 +245,9 @@ class TestEvaluationNN(unittest.TestCase):
 
         loss_functions = create_parametric_loss_functions_from_parameters(template_loss_function=dummy_function,
                                                                           parameters=parameters)
-        algo = instantiate_algorithm_for_learning(loss_function_for_algorithm=lambda x: x**2,
-                                                  loss_functions=loss_functions,
+        algo = instantiate_algorithm_for_learning(loss_functions=loss_functions,
                                                   dimension_of_hyperparameters=10)
+        # Check that we get a PacBayesOptimizationAlgorithm, and the needed parameters are set.
         self.assertIsInstance(algo, PacBayesOptimizationAlgorithm)
         self.assertIsInstance(algo.sufficient_statistics, Callable)
         self.assertIsInstance(algo.natural_parameters, Callable)
@@ -157,100 +255,20 @@ class TestEvaluationNN(unittest.TestCase):
         self.assertIsInstance(algo.epsilon.item(), float)
         self.assertIsInstance(algo.constraint, Constraint)
 
-    @unittest.skipIf(condition=(TESTING_LEVEL != 'FULL_TEST_WITH_EXPERIMENTS'),
-                     reason='Too expensive to test all the time.')
-    def test_parameters_of_experiment(self):
-        number_of_datapoints = get_number_of_datapoints()
-        self.assertEqual(number_of_datapoints, {'prior': 250, 'train': 250, 'test': 250, 'validation': 250})
+    def test_create_folder(self):
+        path = '/home/michael/Desktop/JMLR_New/Experiments/neural_network_training/data/'
+        create_folder_for_storing_data(path)
 
-        initialization_parameters = get_initialization_parameters()
-        self.assertEqual(initialization_parameters,
-                         {'lr': 1e-3, 'num_iter_max': 1000, 'num_iter_print_update': 200,
-                          'num_iter_update_stepsize': 200, 'with_print': True})
+    def test_save_data(self):
+        dummy_savings_path = '/home/michael/Desktop/JMLR_New/Experiments/neural_network_training/dummy_data/'
+        save_data(savings_path=dummy_savings_path,
+                  pac_bound=np.empty(1),
+                  initial_state=np.empty(1),
+                  number_of_iterations=0,
+                  parameters={},
+                  samples_prior=[],
+                  best_sample={})
 
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        fitting_parameters = get_fitting_parameters(maximal_number_of_iterations=maximal_number_of_iterations)
-        self.assertEqual(fitting_parameters,
-                         {'restart_probability': 1/maximal_number_of_iterations,
-                          'length_trajectory': 1,
-                          'n_max': int(100e3),
-                          'lr': 1e-4,
-                          'num_iter_update_stepsize': int(10e3),
-                          'factor_stepsize_update': 0.5})
-
-        estimation_parameters = get_parameters_of_estimation()
-        self.assertEqual(estimation_parameters,
-                         {'quantile_distance': 0.075, 'quantiles': (0.01, 0.99), 'probabilities': (0.95, 1.0)})
-
-        def dummy_statistics(x):
-            return x
-
-        pac_bayes_parameters = get_pac_bayes_parameters(sufficient_statistics=dummy_statistics)
-        self.assertEqual(pac_bayes_parameters,
-                         {'sufficient_statistics': dummy_statistics,
-                          'natural_parameters': evaluate_natural_parameters_at,
-                          'covering_number': 75000, 'epsilon': 0.05, 'n_max': 100}
-                         )
-
-        update_parameters = get_update_parameters()
-        self.assertEqual(update_parameters,
-                         {'num_iter_print_update': 1000,
-                          'with_print': True,
-                          'bins': [1e6, 1e4, 1e2, 1e0, 1e-2, 1e-4, 1e-6, 1e-8, 1e-10][::-1]}
-                         )
-
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        sampling_parameters = get_sampling_parameters(maximal_number_of_iterations)
-        self.assertEqual(sampling_parameters,
-                         {'lr': torch.tensor(1e-6),
-                          'length_trajectory': 1,
-                          'with_restarting': True,
-                          'restart_probability': 1 / maximal_number_of_iterations,
-                          'num_samples': 100,
-                          'num_iter_burnin': 0}
-                         )
-
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        constraint_parameters = get_constraint_parameters(maximal_number_of_iterations)
-        self.assertEqual(constraint_parameters,
-                         {'num_iter_update_constraint': int(maximal_number_of_iterations // 4)}
-                         )
-
-    @unittest.skipIf(condition=(TESTING_LEVEL == 'FULL_TEST_WITH_EXPERIMENTS'),
-                     reason='Too expensive to test all the time.')
-    def test_get_parameters(self):
-
-        number_of_datapoints = get_number_of_datapoints()
-        self.assertIsInstance(number_of_datapoints, dict)
-
-        initialization_parameters = get_initialization_parameters()
-        self.assertIsInstance(initialization_parameters, dict)
-
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        fitting_parameters = get_fitting_parameters(maximal_number_of_iterations=maximal_number_of_iterations)
-        self.assertIsInstance(fitting_parameters, dict)
-
-        estimation_parameters = get_parameters_of_estimation()
-        self.assertIsInstance(estimation_parameters, dict)
-
-        def dummy_statistics(x):
-            return x
-
-        pac_bayes_parameters = get_pac_bayes_parameters(sufficient_statistics=dummy_statistics)
-        self.assertIsInstance(pac_bayes_parameters, dict)
-
-        update_parameters = get_update_parameters()
-        self.assertIsInstance(update_parameters, dict)
-
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        sampling_parameters = get_sampling_parameters(maximal_number_of_iterations)
-        self.assertIsInstance(sampling_parameters, dict)
-
-        maximal_number_of_iterations = torch.randint(low=1, high=100, size=(1,)).item()
-        constraint_parameters = get_constraint_parameters(maximal_number_of_iterations)
-        self.assertIsInstance(constraint_parameters, dict)
-
-    @unittest.skipIf(condition=(TESTING_LEVEL != 'FULL_TEST_WITH_EXPERIMENTS'),
-                     reason='Too expensive to test all the time.')
+    @unittest.skip(reason='Too expensive to test all the time.')
     def test_run_nn_training_experiment(self):
         set_up_and_train_algorithm('/home/michael/Desktop/JMLR_New/Experiments')
