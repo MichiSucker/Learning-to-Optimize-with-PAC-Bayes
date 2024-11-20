a47e17356d115fba597265bc60609ed2458f2919
Michael Sucker <michael_sucker@t-online.de>
2024-11-20 08:37:02 +0100

    Refactoring + more tests.

--- tests/Experiments/CurrentlyTesting/test_data_generation.py
+++ tests/Experiments/CurrentlyTesting/test_data_generation.py
@@ -21,45 +21,52 @@ from experiments.nn_training.data_generation import (get_loss_of_neural_network,
 class TestDataGeneration(unittest.TestCase):
 
     def test_get_criterion(self):
+        # Check that loss of neural network is MSE
         c = get_loss_of_neural_network()
         x, y = torch.rand(size=(100,)), torch.rand(size=(100,))
         self.assertEqual(c(x, y), torch.nn.MSELoss()(x, y))
 
     def test_get_distribution_of_datapoints(self):
+        # Check that data points are sampled uniformly
         d = get_distribution_of_datapoints()
         self.assertIsInstance(d, torch.distributions.uniform.Uniform)
         self.assertEqual(d.low, -2)
         self.assertEqual(d.high, 2)
 
     def test_get_distribution_of_coefficients(self):
+        # Check that coefficients are sampled uniformly
         d = get_distribution_of_coefficients()
         self.assertIsInstance(d, torch.distributions.uniform.Uniform)
         self.assertEqual(d.low, -5)
         self.assertEqual(d.high, 5)
 
     def test_get_powers_of_polynomials(self):
+        # Check that powers of polynomials are specified correctly.
         powers = get_powers_of_polynomials()
         self.assertTrue(torch.equal(powers, torch.arange(6)))
         self.assertTrue(torch.max(powers) == 5)
         self.assertTrue(torch.min(powers) == 0)
 
     def test_get_observations_for_x_values(self):
+        # Check that you get an sorted tensor of x-values.
         number_of_samples = torch.randint(low=1, high=100, size=(1,)).item()
         d = get_distribution_of_datapoints()
         xes = get_observations_for_x_values(number_of_samples=number_of_samples, distribution_x_values=d)
         self.assertEqual(xes.shape, torch.Size((number_of_samples, 1)))
+        self.assertTrue(torch.equal(xes, torch.sort(xes)[0]))
 
     def test_get_coefficients(self):
-        d = get_distribution_of_coefficients()
+        # Check that you get a number for each power of the polynomial
         powers = get_powers_of_polynomials()
-        c = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers))
+        c = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers).item())
         self.assertEqual(len(c), len(powers))
 
     def test_get_ground_truth_values(self):
+        # Check that ground-truth values are computed correctly.
         number_of_samples = torch.randint(low=1, high=100, size=(1,)).item()
         x_values = get_observations_for_x_values(number_of_samples, get_distribution_of_datapoints())
         powers = get_powers_of_polynomials()
-        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers))
+        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers).item())
         gt_values = get_ground_truth_values(x_values=x_values, coefficients=coefficients, powers=powers)
         self.assertEqual(gt_values.shape, torch.Size((number_of_samples, 1)))
         for i, x in enumerate(x_values):
@@ -67,22 +74,26 @@ class TestDataGeneration(unittest.TestCase):
                              gt_values[i])
 
     def test_get_y_values(self):
+        # Check that y-values are ground-truth + standard-normal noise.
         number_of_samples = torch.randint(low=100, high=250, size=(1,)).item()
         x_values = get_observations_for_x_values(number_of_samples, get_distribution_of_datapoints())
         powers = get_powers_of_polynomials()
-        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers))
+        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers).item())
         gt_values = get_ground_truth_values(x_values=x_values, coefficients=coefficients, powers=powers)
         y_values = get_y_values(gt_values)
         self.assertEqual(gt_values.shape, y_values.shape)
         self.assertTrue(torch.mean(gt_values - y_values) < 1)   # This could be made more precise.
 
-    def test_get_parameter(self):
+    def test_create_parameter(self):
+        # Initialize
         number_of_samples = torch.randint(low=100, high=250, size=(1,)).item()
         x_values = get_observations_for_x_values(number_of_samples, get_distribution_of_datapoints())
         powers = get_powers_of_polynomials()
-        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers))
+        coefficients = get_coefficients(get_distribution_of_coefficients(), maximal_degree=torch.max(powers).item())
         gt_values = get_ground_truth_values(x_values=x_values, coefficients=coefficients, powers=powers)
         y_values = get_y_values(gt_values)
+
+        # Check that the parameter has the needed keys.
         p = create_parameter(x_values=x_values, y_values=y_values, ground_truth_values=gt_values,
                              coefficients=coefficients)
         self.assertIsInstance(p, dict)
@@ -91,8 +102,11 @@ class TestDataGeneration(unittest.TestCase):
         self.assertTrue('ground_truth_values' in list(p.keys()))
         self.assertTrue('coefficients' in list(p.keys()))
         self.assertTrue('optimal_loss' in list(p.keys()))
+        self.assertTrue(len(p.keys()) == 5)
 
     def test_check_and_extract_number_of_datapoints(self):
+        # Check that it raises an error if at least one of the data sets is not specified.
+        # And check that the extracted numbers are correct.
         with self.assertRaises(ValueError):
             check_and_extract_number_of_datapoints({})
         with self.assertRaises(ValueError):
@@ -102,9 +116,9 @@ class TestDataGeneration(unittest.TestCase):
         with self.assertRaises(ValueError):
             check_and_extract_number_of_datapoints({'prior': 1, 'train': 1, 'test': 1})
         number_data = {'prior': torch.randint(low=1, high=100, size=(1,)).item(),
-             'train': torch.randint(low=1, high=100, size=(1,)).item(),
-             'test': torch.randint(low=1, high=100, size=(1,)).item(),
-             'validation': torch.randint(low=1, high=100, size=(1,)).item()}
+                       'train': torch.randint(low=1, high=100, size=(1,)).item(),
+                       'test': torch.randint(low=1, high=100, size=(1,)).item(),
+                       'validation': torch.randint(low=1, high=100, size=(1,)).item()}
         n_prior, n_train, n_test, n_val = check_and_extract_number_of_datapoints(number_data)
         self.assertEqual(n_prior, number_data['prior'])
         self.assertEqual(n_train, number_data['train'])
@@ -112,6 +126,7 @@ class TestDataGeneration(unittest.TestCase):
         self.assertEqual(n_val, number_data['validation'])
 
     def test_get_loss_of_algorithm(self):
+        # This is a weak test: Just check that you get a callable function.
         criterion = get_loss_of_neural_network()
 
         def dummy_neural_network(x):
@@ -121,6 +136,8 @@ class TestDataGeneration(unittest.TestCase):
         self.assertIsInstance(loss, Callable)
 
     def test_get_parameters(self):
+        # We only have to check that we get a dictionary, and for each entry in the dictionary we have a list of
+        # parameters.
         number_data = {'prior': torch.randint(low=1, high=100, size=(1,)).item(),
                        'train': torch.randint(low=1, high=100, size=(1,)).item(),
                        'test': torch.randint(low=1, high=100, size=(1,)).item(),
@@ -144,6 +161,8 @@ class TestDataGeneration(unittest.TestCase):
 
     def test_get_data(self):
 
+        # Check that we get a loss-function for the algorithm, a loss-function for the neural network, and a dictionary
+        # of data sets.
         def dummy_neural_network(x):
             return torch.tensor(1.)
 
@@ -162,13 +181,3 @@ class TestDataGeneration(unittest.TestCase):
         self.assertTrue('train' in list(parameters.keys()))
         self.assertTrue('test' in list(parameters.keys()))
         self.assertTrue('validation' in list(parameters.keys()))
-
-        for key in parameters.keys():
-            self.assertIsInstance(parameters[key], list)
-            for p in parameters[key]:
-                self.assertIsInstance(p, dict)
-                self.assertTrue('x_values' in list(p.keys()))
-                self.assertTrue('y_values' in list(p.keys()))
-                self.assertTrue('ground_truth_values' in list(p.keys()))
-                self.assertTrue('coefficients' in list(p.keys()))
-                self.assertTrue('optimal_loss' in list(p.keys()))
