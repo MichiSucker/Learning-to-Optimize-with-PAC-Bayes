329002b2cac9a8017956d6b978b0808e0f14cf22
MichiSucker <michael_sucker@t-online.de>
2024-12-09 07:39:33 +0100

    Added random seeds for exact reproducibility, and some print-statements during run-time.

--- experiments/image_processing/run_experiment.py
+++ experiments/image_processing/run_experiment.py
@@ -14,12 +14,20 @@ def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
 def run(path_to_experiment_folder: str) -> None:
 
     print("Starting image-processing experiment.")
-    torch.manual_seed(27)  # This is for exact reproducibility.
+    # torch.manual_seed(27)  # This is for exact reproducibility.
+    torch.manual_seed(2)   # If you want to reproduce exactly.
+    seed = torch.randint(low=0, high=100, size=(1,)).item()
+    torch.manual_seed(seed)
 
     path_to_images = '/home/michael/Desktop/Experiments/Images/'
     path_of_experiment = create_folder_for_experiment(path_to_experiment_folder)
+    print("\tStarting training.")
     set_up_and_train_algorithm(path_of_experiment=path_of_experiment, path_to_images=path_to_images)
+    print("\tFinished training.")
+    print("\tStarting evaluation.")
     evaluate_algorithm(path_of_experiment=path_of_experiment, loading_path=path_of_experiment + 'data/')
+    print("\tFinished evaluation.")
+    print("\tCreating evaluation plot.")
     create_evaluation_plots(loading_path=path_of_experiment + 'data/', path_of_experiment=path_of_experiment)
 
     print("Finished image-processing experiment.")
--- experiments/lasso/run_experiment.py
+++ experiments/lasso/run_experiment.py
@@ -14,14 +14,22 @@ def create_folder_for_experiment(path_to_experiment_folder: str) -> str:
 def run(path_to_experiment_folder: str) -> None:
 
     print("Starting lasso experiment.")
-    torch.manual_seed(37)   # This is for exact reproducibility.
+    # torch.manual_seed(37)   # This is for exact reproducibility.
+    torch.manual_seed(3)   # If you want to reproduce exactly.
+    seed = torch.randint(low=0, high=100, size=(1,)).item()
+    torch.manual_seed(seed)
 
     # This is pretty important again. Also, it makes sure that all tensor types do match.
     torch.set_default_dtype(torch.float64)
 
     path_of_experiment = create_folder_for_experiment(path_to_experiment_folder)
+    print("\tStarting training.")
     set_up_and_train_algorithm(path_of_experiment=path_of_experiment)
+    print("\tFinished training.")
+    print("\tStarting evaluation.")
     evaluate_algorithm(path_of_experiment=path_of_experiment, loading_path=path_of_experiment + 'data/')
+    print("\tFinished evaluation.")
+    print("\tCreating evaluation plot.")
     create_evaluation_plots(loading_path=path_of_experiment + 'data/', path_of_experiment=path_of_experiment)
 
     print("Finished lasso experiment.")
